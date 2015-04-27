0f1246c208ddcc5cc999bf44656264610a96f02c
kinoue <kinoue@2cc7cdd0-1db9-4218-aa4a-1d8dad43e0f0>
2015-04-27 00:35:40 +0000

    MISC: Indent fix
    git-svn-id: http://my.redmine.jp/svn/iric/v3.X/gui/trunk@5228 2cc7cdd0-1db9-4218-aa4a-1d8dad43e0f0

--- libs/misc/versionnumber.cpp
+++ libs/misc/versionnumber.cpp
@@ -3,25 +3,26 @@
 
 const QString VersionNumber::toString()const
 {
-    QString ret;
-    QString major; major.setNum(m_major);
-    ret.append(major);
-    ret.append(".");
-    QString minor; minor.setNum(m_minor);
-    ret.append(minor);
+	QString ret;
+	QString major; major.setNum(m_major);
+	ret.append(major);
+	ret.append(".");
+	QString minor; minor.setNum(m_minor);
+	ret.append(minor);
 
-    if (m_fix != 0 || m_build != 0){
-        ret.append(".");
-        QString fix; fix.setNum(m_fix);
-        ret.append(fix);
-    }
-    if (m_build != 0){
-        ret.append(".");
-        QString build;
+	if (m_fix != 0 || m_build != 0){
+		ret.append(".");
+		QString fix;
+		fix.setNum(m_fix);
+		ret.append(fix);
+	}
+	if (m_build != 0){
+		ret.append(".");
+		QString build;
 		build.setNum(m_build);
-        ret.append(build);
-    }
-    return ret;
+		ret.append(build);
+	}
+	return ret;
 }
 
 const QString VersionNumber::toAboutString() const
@@ -46,37 +47,40 @@ const QString VersionNumber::toAboutString() const
 
 void VersionNumber::fromString(const QString& version)
 {
-    init();
-    QStringList versions = version.split(".");
-    m_major = versions.front().toUInt();
-    versions.pop_front();
-    if (versions.empty()){return;}
-    m_minor = versions.front().toUInt();
-    versions.pop_front();
-    if (versions.empty()){return;}
-    m_fix = versions.front().toUInt();
-    versions.pop_front();
-    if (versions.empty()){return;}
-    m_build = versions.front().toUInt();
+	init();
+	QStringList versions = version.split(".");
+	m_major = versions.front().toUInt();
+	versions.pop_front();
+	if (versions.empty()){return;}
+
+	m_minor = versions.front().toUInt();
+	versions.pop_front();
+	if (versions.empty()){return;}
+
+	m_fix = versions.front().toUInt();
+	versions.pop_front();
+	if (versions.empty()){return;}
+
+	m_build = versions.front().toUInt();
 }
 
 bool VersionNumber::compatibleWith(const VersionNumber& v)const{
-    if (major() != v.major()){return false;}
+	if (major() != v.major()){return false;}
 	if (minor() < v.minor()){return false;}
-    return true;
+	return true;
 }
 
 bool VersionNumber::operator ==(const VersionNumber& v) const{
 	return
-			(m_major == v.m_major) &&
-			(m_minor == v.m_minor) &&
-			(m_fix == v.m_fix) &&
-			(m_build == v.m_build);
+		(m_major == v.m_major) &&
+		(m_minor == v.m_minor) &&
+		(m_fix == v.m_fix) &&
+		(m_build == v.m_build);
 }
 
 void VersionNumber::init(){
-    m_major = 0;
-    m_minor = 0;
-    m_fix = 0;
-    m_build = 0;
+	m_major = 0;
+	m_minor = 0;
+	m_fix = 0;
+	m_build = 0;
 }
--- libs/misc/versionnumber.h
+++ libs/misc/versionnumber.h
@@ -26,63 +26,63 @@
 class MISCDLL_EXPORT VersionNumber
 {
 public:
-		/// Constructor
-		VersionNumber(){
-				init();
-		}
-		/// Constructor (with initialization)
-		VersionNumber(unsigned int major, unsigned int minor = 0, unsigned int fix = 0, unsigned int build = 0){
-				init();
-				m_major = major;
-				m_minor = minor;
-				m_fix = fix;
-				m_build = build;
-		}
-		/// Constructor (with initialization with string version number)
-		VersionNumber(const QString& version){
-				fromString(version);
-		}
-		/// Major version number
-		unsigned int major()const{return m_major;}
-		/// Minor version number
-		unsigned int minor()const{return m_minor;}
-		/// Fix version number
-		unsigned int fix()const{return m_fix;}
-		/// Build number
-		unsigned int build()const{return m_build;}
-		/// Set major version number
-		void setMajor(unsigned int major){m_major = major;}
-		/// Set minor version number
-		void setMinor(unsigned int minor){m_minor = minor;}
-		/// Set fix version number
-		void setFix(unsigned int fix){m_fix = fix;}
-		/// Set build number
-		void setBuild(unsigned int build){m_build = build;}
-		/// Full version number as string
-		const QString toString()const;
+	/// Constructor
+	VersionNumber(){
+		init();
+	}
+	/// Constructor (with initialization)
+	VersionNumber(unsigned int major, unsigned int minor = 0, unsigned int fix = 0, unsigned int build = 0){
+		init();
+		m_major = major;
+		m_minor = minor;
+		m_fix = fix;
+		m_build = build;
+	}
+	/// Constructor (with initialization with string version number)
+	VersionNumber(const QString& version){
+		fromString(version);
+	}
+	/// Major version number
+	unsigned int major()const{return m_major;}
+	/// Minor version number
+	unsigned int minor()const{return m_minor;}
+	/// Fix version number
+	unsigned int fix()const{return m_fix;}
+	/// Build number
+	unsigned int build()const{return m_build;}
+	/// Set major version number
+	void setMajor(unsigned int major){m_major = major;}
+	/// Set minor version number
+	void setMinor(unsigned int minor){m_minor = minor;}
+	/// Set fix version number
+	void setFix(unsigned int fix){m_fix = fix;}
+	/// Set build number
+	void setBuild(unsigned int build){m_build = build;}
+	/// Full version number as string
+	const QString toString()const;
 	/// Version number as string (for about dialog)
 	const QString toAboutString() const;
-		/// Set version number as string
-		void fromString(const QString& version);
-		/// Return whether the target version is compatible with this version.
-		/**
-			* Compatibility is defined in the following logic:
-			* - If the major version is different, it is not compatible with
-			*   the target version.
-			* - if the minor version is smaller than the target version, it is
-			*   not compatible with that.
-			*/
+	/// Set version number as string
+	void fromString(const QString& version);
+	/// Return whether the target version is compatible with this version.
+	/**
+	  * Compatibility is defined in the following logic:
+	  * - If the major version is different, it is not compatible with
+	  *   the target version.
+	  * - if the minor version is smaller than the target version, it is
+	  *   not compatible with that.
+	  */
 	bool compatibleWith(const VersionNumber& v) const;
 	bool operator ==(const VersionNumber& v) const;
 	bool operator !=(const VersionNumber& v) const {
 		return ! (operator==(v));
 	}
 private:
-		void init();
-		unsigned int m_major;
-		unsigned int m_minor;
-		unsigned int m_fix;
-		unsigned int m_build;
+	void init();
+	unsigned int m_major;
+	unsigned int m_minor;
+	unsigned int m_fix;
+	unsigned int m_build;
 };
 
 #endif // VERSIONNUMBER_H
