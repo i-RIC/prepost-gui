210b9ddbff1af760274e46b4dc68be9d78084b50
Keisuke Inoue <keisuke.inoue@mizuho-ir.co.jp>
2016-10-07 19:54:20 +0900

    Calculation Condition I/O function definition

--- iriclib/iriclib_cgnsfile.h
+++ iriclib/iriclib_cgnsfile.h
@@ -26,6 +26,24 @@ public:
 
 	int Set_ZoneId(int zoneid);
 
+	// ------------------------------
+	// Calculation Condition Reading
+	// ------------------------------
+	int CC_CC_Read_Integer(char* name, int* intvalue);
+	int CC_Read_Real(char* name, double* realvalue);
+	int CC_Read_RealSingle(char* name, float* realvalue);
+	int CC_Read_StringLen(char* name, int* length);
+	int CC_Read_String(char* name, char* strvalue);
+	int CC_Read_FunctionalSize(char* name, cgsize_t* size);
+	int CC_Read_Functional(char* name, double* x, double* y);
+	int CC_Read_FunctionalWithName(char* name, char* paramname, double* data);
+	int CC_Read_Functional_RealSingle(char* name, float* x, float* y);
+
+	int CC_Write_Integer(char* name, int intvalue);
+	int CC_Write_Real(char* name, double realvalue);
+	int CC_Write_String(char* name, char* strvalue);
+	int CC_Write_Functional(char* name, int length, double* realarray_x, double* realarray_y);
+	int CC_Write_FunctionalWithName(char* name, char* paramname, int length, double* data);
 
 private:
 	Impl* impl;
