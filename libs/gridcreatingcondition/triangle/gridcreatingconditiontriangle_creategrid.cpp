de48983c6ff5c1aed734fdcfe74783636a6d676a
Keisuke Inoue <keisuke.inoue@mizuho-ir.co.jp>
2017-08-17 17:32:45 +0900

    Refactoring: setupTriangleInput() added

--- libs/gridcreatingcondition/triangle/gridcreatingconditiontriangle_creategrid.cpp
+++ libs/gridcreatingcondition/triangle/gridcreatingconditiontriangle_creategrid.cpp
@@ -26,6 +26,139 @@
 
 namespace {
 
+void setupTriangleInput(GridCreatingConditionTriangleGridRegionPolygon* regionPolygon,
+												const QList<GridCreatingConditionTriangleRemeshPolygon*>& remeshPolygons,
+												const QList<GridCreatingConditionTriangleHolePolygon*>& holePolygons,
+												const QList<GridCreatingConditionTriangleDivisionLine*>& divisionLines,
+												bool areaConstraint, double area,
+												const QPointF& offset,
+												triangulateio* in)
+{
+	QPolygonF pol = regionPolygon->polygon();
+	QVector<QPointF> line;
+	int pointCount = 0;
+	int segmentCount = 0;
+	int regionCount = 1;
+	pointCount += (pol.count() - 1);
+	segmentCount += (pol.count() - 1);
+	for (int i = 0; i < remeshPolygons.size(); ++i) {
+		pol = remeshPolygons[i]->polygon();
+		pointCount += (pol.count() - 1);
+		segmentCount += (pol.count() - 1);
+		++ regionCount;
+	}
+	for (int i = 0; i < holePolygons.size(); ++i) {
+		pol = holePolygons[i]->polygon();
+		pointCount += (pol.count() - 1);
+		segmentCount += (pol.count() - 1);
+	}
+	for (int i = 0; i < divisionLines.size(); ++i) {
+		line = divisionLines[i]->polyLine();
+		pointCount += line.count();
+		segmentCount += (line.count() - 1);
+	}
+
+	in->pointlist = new double[pointCount * 2];
+	in->pointattributelist = NULL;
+	in->pointmarkerlist = NULL;
+	in->numberofpoints = pointCount;
+	in->numberofpointattributes = 0;
+
+	in->trianglelist = NULL;
+	in->triangleattributelist = NULL;
+	in->trianglearealist = NULL;
+	in->neighborlist = NULL;
+	in->numberoftriangles = 0;
+	in->numberofcorners = 0;
+	in->numberoftriangleattributes = 0;
+
+	in->segmentlist = new int[segmentCount * 2];
+	in->segmentmarkerlist = NULL;
+	in->numberofsegments = segmentCount;
+
+	in->holelist = new double[holePolygons.count() * 2];
+	in->numberofholes = holePolygons.count();
+
+	in->regionlist = new double[regionCount * 4];
+	in->numberofregions = regionCount;
+
+	in->edgelist = NULL;
+	in->edgemarkerlist = NULL;
+	in->normlist = NULL;
+	in->numberofedges = 0;
+
+	pol = regionPolygon->polygon(offset);
+	for (int i = 0; i < pol.count() - 1; ++i) {
+		*(in->pointlist + i * 2)     = pol.at(i).x();
+		*(in->pointlist + i * 2 + 1) = pol.at(i).y();
+		*(in->segmentlist + i * 2) = i + 1;
+		*(in->segmentlist + i * 2 + 1) = (i + 1) % (pol.count() - 1) + 1;
+	}
+	QPointF innerP = regionPolygon->innerPoint(offset);
+	*(in->regionlist)     = innerP.x();
+	*(in->regionlist + 1) = innerP.y();
+	*(in->regionlist + 2) = 0;
+	if (areaConstraint) {
+		*(in->regionlist + 3) = area;
+	} else {
+		// no area constraint.
+		QRectF rect = pol.boundingRect();
+		*(in->regionlist + 3) = rect.width() * rect.height();
+	}
+	int pOffset = 2 * (pol.count() - 1);
+	int sOffset = (pol.count() - 1);
+	int aOffset = 1;
+	for (int i = 0; i < remeshPolygons.size(); ++i) {
+		GridCreatingConditionTriangleRemeshPolygon* rpol = remeshPolygons[i];
+		pol = rpol->polygon(offset);
+		for (int j = 0; j < pol.count() - 1; ++j) {
+			*(in->pointlist + j * 2 + pOffset) = pol.at(j).x();
+			*(in->pointlist + j * 2 + 1 + pOffset) = pol.at(j).y();
+			*(in->segmentlist + j * 2 + sOffset * 2) = j + sOffset + 1;
+			*(in->segmentlist + j * 2 + 1 + sOffset * 2) = (j + 1) % (pol.count() - 1) + sOffset + 1;
+		}
+		innerP = rpol->innerPoint(offset);
+		*(in->regionlist + 4 * aOffset) = innerP.x();
+		*(in->regionlist + 4 * aOffset + 1) = innerP.y();
+		*(in->regionlist + 4 * aOffset + 2) = 0;
+		*(in->regionlist + 4 * aOffset + 3) = rpol->cellSize();
+		pOffset += 2 * (pol.count() - 1);
+		sOffset += (pol.count() - 1);
+		++aOffset;
+	}
+
+	for (int i = 0; i < holePolygons.size(); ++i) {
+		GridCreatingConditionTriangleHolePolygon* hpol = holePolygons[i];
+		pol = hpol->polygon(offset);
+		for (int j = 0; j < pol.count() - 1; ++j) {
+			*(in->pointlist + j * 2 + pOffset) = pol.at(j).x();
+			*(in->pointlist + j * 2 + 1 + pOffset) = pol.at(j).y();
+			*(in->segmentlist + j * 2 + sOffset * 2) = j + sOffset + 1;
+			*(in->segmentlist + j * 2 + 1 + sOffset * 2) = (j + 1) % (pol.count() - 1) + sOffset + 1;
+		}
+		innerP = hpol->innerPoint(offset);
+		*(in->holelist + i * 2) = innerP.x();
+		*(in->holelist + i * 2 + 1) = innerP.y();
+		pOffset += 2 * (pol.count() - 1);
+		sOffset += (pol.count() - 1);
+	}
+
+	for (int i = 0; i < divisionLines.size(); ++i) {
+		GridCreatingConditionTriangleDivisionLine* line = divisionLines[i];
+		QVector<QPointF> l = line->polyLine(offset);
+		for (int j = 0; j < l.count(); ++j) {
+			*(in->pointlist + j * 2 + pOffset)     = l.at(j).x();
+			*(in->pointlist + j * 2 + 1 + pOffset) = l.at(j).y();
+			if (j != 0) {
+				*(in->segmentlist + (j - 1) * 2 + sOffset * 2) = j + pOffset / 2;
+				*(in->segmentlist + (j - 1) * 2 + 1 + sOffset * 2) = j + 1 + pOffset / 2;
+			}
+		}
+		pOffset += 2 * l.count();
+		sOffset += (l.count() - 1);
+	}
+}
+
 void clearOutputIO(triangulateio* out)
 {
 	out->pointlist = NULL;
@@ -51,7 +184,7 @@ WaitDialog* setupWaitDialog(QWidget* parent, int progress)
 	dialog->setRange(0, 100);
 	dialog->setUnknownLimitMode(300);
 	dialog->setProgress(progress);
-	dialog->setMessage(tr("Generating grid..."));
+	dialog->setMessage(GridCreatingConditionTriangle::tr("Generating grid..."));
 	return dialog;
 }
 
@@ -142,129 +275,11 @@ Grid* GridCreatingConditionTriangle::createGrid()
 	Unstructured2DGrid* grid = new Unstructured2DGrid(nullptr);
 	PreProcessorGridTypeDataItemInterface* gt = dynamic_cast<PreProcessorGridTypeDataItemInterface*>(m_conditionDataItem->parent()->parent());
 	gt->gridType()->buildGridAttributes(grid);
-	QPolygonF pol = m_gridRegionPolygon->polygon();
-	QVector<QPointF> line;
-	int pointCount = 0;
-	int segmentCount = 0;
-	int regionCount = 1;
-	pointCount += (pol.count() - 1);
-	segmentCount += (pol.count() - 1);
-	QPointF offset = pol.boundingRect().topLeft();
-	for (int i = 0; i < m_remeshPolygons.count(); ++i) {
-		pol = m_remeshPolygons[i]->polygon();
-		pointCount += (pol.count() - 1);
-		segmentCount += (pol.count() - 1);
-		++regionCount;
-	}
-	for (int i = 0; i < m_holePolygons.count(); ++i) {
-		pol = m_holePolygons[i]->polygon();
-		pointCount += (pol.count() - 1);
-		segmentCount += (pol.count() - 1);
-	}
-	for (int i = 0; i < m_divisionLines.count(); ++i) {
-		line = m_divisionLines[i]->polyLine();
-		pointCount += line.count();
-		segmentCount += (line.count() - 1);
-	}
+
 	triangulateio in, out;
 
-	in.pointlist = new double[pointCount * 2];
-	in.pointattributelist = NULL;
-	in.pointmarkerlist = NULL;
-	in.numberofpoints = pointCount;
-	in.numberofpointattributes = 0;
-
-	in.trianglelist = NULL;
-	in.triangleattributelist = NULL;
-	in.trianglearealist = NULL;
-	in.neighborlist = NULL;
-	in.numberoftriangles = 0;
-	in.numberofcorners = 0;
-	in.numberoftriangleattributes = 0;
-
-	in.segmentlist = new int[segmentCount * 2];
-	in.segmentmarkerlist = NULL;
-	in.numberofsegments = segmentCount;
-
-	in.holelist = new double[m_holePolygons.count() * 2];
-	in.numberofholes = m_holePolygons.count();
-
-	in.regionlist = new double[regionCount * 4];
-	in.numberofregions = regionCount;
-
-	in.edgelist = NULL;
-	in.edgemarkerlist = NULL;
-	in.normlist = NULL;
-	in.numberofedges = 0;
-
-	pol = m_gridRegionPolygon->polygon(offset);
-	for (int i = 0; i < pol.count() - 1; ++i) {
-		*(in.pointlist + i * 2)     = pol.at(i).x();
-		*(in.pointlist + i * 2 + 1) = pol.at(i).y();
-		*(in.segmentlist + i * 2) = i + 1;
-		*(in.segmentlist + i * 2 + 1) = (i + 1) % (pol.count() - 1) + 1;
-	}
-	QPointF innerP = m_gridRegionPolygon->innerPoint(offset);
-	*(in.regionlist)     = innerP.x();
-	*(in.regionlist + 1) = innerP.y();
-	*(in.regionlist + 2) = 0;
-	if (m_areaConstraint) {
-		*(in.regionlist + 3) = m_area;
-	} else {
-		// no area constraint.
-		QRectF rect = pol.boundingRect();
-		*(in.regionlist + 3) = rect.width() * rect.height();
-	}
-	int pOffset = 2 * (pol.count() - 1);
-	int sOffset = (pol.count() - 1);
-	int aOffset = 1;
-	for (int i = 0; i < m_remeshPolygons.count(); ++i) {
-		GridCreatingConditionTriangleRemeshPolygon* rpol = m_remeshPolygons[i];
-		pol = rpol->polygon(offset);
-		for (int j = 0; j < pol.count() - 1; ++j) {
-			*(in.pointlist + j * 2 + pOffset)     = pol.at(j).x();
-			*(in.pointlist + j * 2 + 1 + pOffset) = pol.at(j).y();
-			*(in.segmentlist + j * 2 + sOffset * 2) = j + sOffset + 1;
-			*(in.segmentlist + j * 2 + 1 + sOffset * 2) = (j + 1) % (pol.count() - 1) + sOffset + 1;
-		}
-		innerP = rpol->innerPoint(offset);
-		*(in.regionlist + 4 * aOffset) = innerP.x();
-		*(in.regionlist + 4 * aOffset + 1) = innerP.y();
-		*(in.regionlist + 4 * aOffset + 2) = 0;
-		*(in.regionlist + 4 * aOffset + 3) = rpol->cellSize();
-		pOffset += 2 * (pol.count() - 1);
-		sOffset += (pol.count() - 1);
-		++aOffset;
-	}
-	for (int i = 0; i < m_holePolygons.count(); ++i) {
-		GridCreatingConditionTriangleHolePolygon* hpol = m_holePolygons[i];
-		pol = hpol->polygon(offset);
-		for (int j = 0; j < pol.count() - 1; ++j) {
-			*(in.pointlist + j * 2 + pOffset)     = pol.at(j).x();
-			*(in.pointlist + j * 2 + 1 + pOffset) = pol.at(j).y();
-			*(in.segmentlist + j * 2 + sOffset * 2) = j + sOffset + 1;
-			*(in.segmentlist + j * 2 + 1 + sOffset * 2) = (j + 1) % (pol.count() - 1) + sOffset + 1;
-		}
-		innerP = hpol->innerPoint(offset);
-		*(in.holelist + i * 2) = innerP.x();
-		*(in.holelist + i * 2 + 1) = innerP.y();
-		pOffset += 2 * (pol.count() - 1);
-		sOffset += (pol.count() - 1);
-	}
-	for (int i = 0; i < m_divisionLines.count(); ++i) {
-		GridCreatingConditionTriangleDivisionLine* line = m_divisionLines[i];
-		QVector<QPointF> l = line->polyLine(offset);
-		for (int j = 0; j < l.count(); ++j) {
-			*(in.pointlist + j * 2 + pOffset)     = l.at(j).x();
-			*(in.pointlist + j * 2 + 1 + pOffset) = l.at(j).y();
-			if (j != 0) {
-				*(in.segmentlist + (j - 1) * 2 + sOffset * 2) = j + pOffset / 2;
-				*(in.segmentlist + (j - 1) * 2 + 1 + sOffset * 2) = j + 1 + pOffset / 2;
-			}
-		}
-		pOffset += 2 * l.count();
-		sOffset += (l.count() - 1);
-	}
+	QPointF offset = m_gridRegionPolygon->polygon().boundingRect().topLeft();
+	setupTriangleInput(m_gridRegionPolygon, m_remeshPolygons, m_holePolygons, m_divisionLines, m_areaConstraint, m_area, offset, &in);
 
 	clearOutputIO(&out);
 
