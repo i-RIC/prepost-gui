bf2acbd9f213ba846d4b727b1ff9efd86ec9cd53
Keisuke Inoue <keisuke.inoue@mizuho-ir.co.jp>
2017-10-10 16:12:21 +0900

    Japanese dictionary updated.

--- libs/solverconsole/languages/iricSolverconsole_th_TH.ts
+++ libs/solverconsole/languages/iricSolverconsole_th_TH.ts
@@ -4,115 +4,121 @@
 <context>
     <name>SolverConsoleWindow</name>
     <message>
-        <location filename="../solverconsolewindow.cpp" line="37"/>
+        <location filename="../solverconsolewindow.cpp" line="38"/>
         <source>&amp;Export solver console log...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../solverconsolewindow.cpp" line="131"/>
+        <location filename="../solverconsolewindow.cpp" line="132"/>
         <source>Show Detail</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../solverconsolewindow.cpp" line="132"/>
-        <location filename="../solverconsolewindow.cpp" line="143"/>
+        <location filename="../solverconsolewindow.cpp" line="133"/>
+        <location filename="../solverconsolewindow.cpp" line="144"/>
         <source>Warning</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../solverconsolewindow.cpp" line="132"/>
+        <location filename="../solverconsolewindow.cpp" line="133"/>
         <source>The following problems found in the grid(s). Do you really want to run the solver with this grid?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../solverconsolewindow.cpp" line="136"/>
+        <location filename="../solverconsolewindow.cpp" line="137"/>
         <source>The simulation has result</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../solverconsolewindow.cpp" line="136"/>
+        <location filename="../solverconsolewindow.cpp" line="137"/>
         <source>Current simulation already has result data. When you run the solver, the current result data is discarded.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../solverconsolewindow.cpp" line="143"/>
+        <location filename="../solverconsolewindow.cpp" line="144"/>
         <source>Error occured. %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../solverconsolewindow.cpp" line="116"/>
-        <location filename="../solverconsolewindow.cpp" line="147"/>
+        <location filename="../solverconsolewindow.cpp" line="117"/>
+        <location filename="../solverconsolewindow.cpp" line="148"/>
         <source>Information</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../solverconsolewindow.cpp" line="116"/>
+        <location filename="../solverconsolewindow.cpp" line="117"/>
         <source>This project is opened in post only mode. You can not run the solver.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../solverconsolewindow.cpp" line="147"/>
+        <location filename="../solverconsolewindow.cpp" line="148"/>
         <source>We recommend that you save the project before starting the solver. Do you want to save?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../solverconsolewindow.cpp" line="161"/>
+        <location filename="../solverconsolewindow.cpp" line="162"/>
         <source>Error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../solverconsolewindow.cpp" line="161"/>
+        <location filename="../solverconsolewindow.cpp" line="162"/>
         <source>Solver executable file %1 does not exists.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../solverconsolewindow.cpp" line="182"/>
+        <location filename="../solverconsolewindow.cpp" line="183"/>
+        <location filename="../solverconsolewindow.cpp" line="344"/>
         <source>Confirm Solver Termination</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../solverconsolewindow.cpp" line="183"/>
+        <location filename="../solverconsolewindow.cpp" line="184"/>
         <source>Do you really want to kill the solver?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../solverconsolewindow.cpp" line="235"/>
-        <location filename="../solverconsolewindow.cpp" line="238"/>
+        <location filename="../solverconsolewindow.cpp" line="236"/>
+        <location filename="../solverconsolewindow.cpp" line="239"/>
         <source>Solver Finished</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../solverconsolewindow.cpp" line="235"/>
+        <location filename="../solverconsolewindow.cpp" line="236"/>
         <source>The solver finished calculation.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../solverconsolewindow.cpp" line="238"/>
+        <location filename="../solverconsolewindow.cpp" line="239"/>
         <source>The solver finished abnormally.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../solverconsolewindow.cpp" line="247"/>
+        <location filename="../solverconsolewindow.cpp" line="248"/>
         <source>Solver Console</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../solverconsolewindow.cpp" line="253"/>
+        <location filename="../solverconsolewindow.cpp" line="254"/>
         <source>running</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../solverconsolewindow.cpp" line="255"/>
+        <location filename="../solverconsolewindow.cpp" line="256"/>
         <source>stopped</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../solverconsolewindow.cpp" line="257"/>
+        <location filename="../solverconsolewindow.cpp" line="258"/>
         <source>Solver Console [%1] (%2)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../solverconsolewindow.cpp" line="348"/>
+        <location filename="../solverconsolewindow.cpp" line="345"/>
+        <source>30 seconds have passed, but the solver do not end. Do you want to kill the solver?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../solverconsolewindow.cpp" line="369"/>
         <source>Background Color</source>
         <translation type="unfinished"></translation>
     </message>
