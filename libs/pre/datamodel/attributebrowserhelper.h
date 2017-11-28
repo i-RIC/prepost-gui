fc7254fa911ca8c1b3009c5a040ec2c87514647f
Keisuke Inoue <kskinoue@gmail.com>
2017-11-28 16:08:41 +0900

    AttributeBrowserHelper class is added.

---
+++ libs/pre/datamodel/attributebrowserhelper.h
@@ -0,0 +1,15 @@
+#ifndef ATTRIBUTEBROWSERHELPER_H
+#define ATTRIBUTEBROWSERHELPER_H
+
+class PreProcessorGridTypeDataItem;
+
+class AttributeBrowserHelper
+{
+public:
+	static bool isAttributeBrowserAvailable(PreProcessorGridTypeDataItem* ditem);
+
+private:
+	AttributeBrowserHelper();
+};
+
+#endif // ATTRIBUTEBROWSERHELPER_H
