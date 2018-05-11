9ac7797efbdb1442bbd8fa2316cca1797cf9e0bf
Keisuke Inoue <kskinoue@gmail.com>
2018-05-11 16:38:37 +0900

    GeoDataPolygon misc refactoring to make if statements shallower.

--- libs/geodata/polygon/geodatapolygon.cpp
+++ libs/geodata/polygon/geodatapolygon.cpp
@@ -268,13 +268,11 @@ void GeoDataPolygon::viewOperationEnded(PreProcessorGraphicsViewInterface* v)
 
 void GeoDataPolygon::keyPressEvent(QKeyEvent* event, PreProcessorGraphicsViewInterface* /*v*/)
 {
-	if (iRIC::isEnterKey(event->key())) {
-		if (m_mouseEventMode == meDefining) {
-			if (m_selectMode == smPolygon) {
-				definePolygon(false);
-			}
-		}
-	}
+	if (! iRIC::isEnterKey(event->key())) {return;}
+	if (m_mouseEventMode != meDefining) {return;}
+	if (m_selectMode != smPolygon) {return;}
+
+	definePolygon(false);
 }
 
 void GeoDataPolygon::keyReleaseEvent(QKeyEvent* /*event*/, PreProcessorGraphicsViewInterface* /*v*/)
