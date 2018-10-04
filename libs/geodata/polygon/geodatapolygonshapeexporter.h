bab2c4d854ee7164260b559028344658633cbfc7
Keisuke Inoue <kskinoue@gmail.com>
2018-10-05 00:19:59 +0900

    Refactoring: Empty lines added to make the header easier to read.

--- libs/geodata/polygon/geodatapolygonshapeexporter.h
+++ libs/geodata/polygon/geodatapolygonshapeexporter.h
@@ -14,8 +14,10 @@ class GD_POLYGON_EXPORT GeoDataPolygonShapeExporter : public GeoDataExporter
 
 public:
 	GeoDataPolygonShapeExporter(GeoDataCreator* creator);
+
 	bool doExport(GeoData* data, const QString& filename, const QString& selectedFilter, QWidget* w, ProjectData* pd) override;
 	const QStringList fileDialogFilters() override;
+
 	SHPHandle getSHPHandle(QString filename);
 	DBFHandle getDBFHandle(QString filename, SolverDefinitionGridAttribute* cond, bool* isDouble);
 	SHPObject* getSHPObject(GeoDataPolygon* polygon, SHPHandle shp, int index, double xoffset = 0, double yoffset = 0);
