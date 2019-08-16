b149c727b88824d4e4e468317df68102e3579286
Keisuke Inoue <kskinoue@gmail.com>
2019-08-16 22:40:00 +0900

    Closes i-RIC/prepost-gui#547

---
+++ plugins/gridexporter/gridlandxmlexporter/gridlandxmlexporter.h
@@ -0,0 +1,22 @@
+#ifndef GRIDLANDXMLEXPORTER_H
+#define GRIDLANDXMLEXPORTER_H
+
+#include <guicore/pre/grid/gridexporterinterface.h>
+#include <QObject>
+
+class GridLandXmlExporter : public QObject, public GridExporterInterface
+{
+	Q_OBJECT
+	Q_PLUGIN_METADATA(IID GridExporterInterface_iid FILE "extrafilters.json")
+	Q_INTERFACES(GridExporterInterface)
+public:
+	GridLandXmlExporter();
+	~GridLandXmlExporter();
+
+	QString caption() const override;
+	bool isGridTypeSupported(SolverDefinitionGridType::GridType gt) const override;
+	QStringList fileDialogFilters() const override;
+	bool doExport(Grid* grid, const QString& filename, const QString& selectedFilter, QWidget* parent) override;
+};
+
+#endif // GRIDLANDXMLEXPORTER_H
---
+++ plugins/gridexporter/gridlandxmlexporter/languages/gridlandxmlexporter_ru_RU.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>GridLandXmlExporter</name>
+    <message>
+        <location filename="../gridlandxmlexporter.cpp" line="29"/>
+        <source>LandXml file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gridlandxmlexporter.cpp" line="48"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gridlandxmlexporter.cpp" line="48"/>
+        <source>Error occured while exporting to %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../gridlandxmlexporter.cpp" line="40"/>
+        <source>LandXml files (*.xml)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
