434461f26a765e297db4c4e17da3814c0d4f868b
Keisuke Inoue <kskinoue@gmail.com>
2019-10-25 11:06:51 +0900

    Refactoring: if statement structure simplified

--- plugins/gridexporter/structured2dgridnayscsvexporter/structured2dgridnayscsvexporter.cpp
+++ plugins/gridexporter/structured2dgridnayscsvexporter/structured2dgridnayscsvexporter.cpp
@@ -40,47 +40,44 @@ bool Structured2DGridNaysCSVExporter::doExport(Grid* grid, const QString& filena
 	int kmax = 2;
 
 	QFile f(filename);
-	if (f.open(QFile::WriteOnly | QFile::Truncate | QIODevice::Text)){
-		GridAttributeContainer* c = grid2d->gridAttribute("Elevation");
-		if (c == nullptr){
-			// this grid does not have Elevation node. Impossible to export data.
-			return false;
-		}
-		GridAttributeRealNodeContainer* container = dynamic_cast<GridAttributeRealNodeContainer*>(c);
+	if (! f.open(QFile::WriteOnly | QFile::Truncate | QIODevice::Text)){return false;}
 
-		c = grid2d->gridAttribute("CellCondition");
-		GridAttributeIntegerCellContainer* cellcond = dynamic_cast<GridAttributeIntegerCellContainer*>(c);
-		QTextStream o(&f);
-		o.setRealNumberPrecision(8);
-		o << "IMAX,JMAX,KMAX " << endl;
-		o << imax << "," << jmax << "," << kmax << endl;
-
-		o << "I,J,K,X,Y,Z" << endl;
-		o << scientific;
-		for (int k = 0; k < kmax; k++){
-			for (int j = 0; j < jmax; j++){
-				for (int i = 0; i < imax; i++){
-					o << i << "," << j << "," << k << ","
-					  << grid2d->vertex(i, j).x() << ","
-					  << grid2d->vertex(i, j).y() << ","
-					  << container->value(grid2d->vertexIndex(i, j)) << endl;
-				}
-			}
-		}
-		if (cellcond != 0){
-			o << "I , J , K , FLAGS" << endl;
-			for (int k = 0; k < kmax - 1; k++){
-				for (int j = 0; j < jmax - 1; j++){
-					for (int i = 0; i < imax - 1; i++){
-						o << i << "," << j << "," << k << ","
-						  << cellcond->value(grid2d->cellIndex(i, j)) << endl;
-					}
-				}
-			}
-		}
-	}else{
-		// file not opened successfully.
+	GridAttributeContainer* c = grid2d->gridAttribute("Elevation");
+	if (c == nullptr){
+		// this grid does not have Elevation node. Impossible to export data.
 		return false;
+	}
+	GridAttributeRealNodeContainer* container = dynamic_cast<GridAttributeRealNodeContainer*>(c);
+
+	c = grid2d->gridAttribute("CellCondition");
+	GridAttributeIntegerCellContainer* cellcond = dynamic_cast<GridAttributeIntegerCellContainer*>(c);
+	QTextStream o(&f);
+	o.setRealNumberPrecision(8);
+	o << "IMAX,JMAX,KMAX " << endl;
+	o << imax << "," << jmax << "," << kmax << endl;
+
+	o << "I,J,K,X,Y,Z" << endl;
+	o << scientific;
+	for (int k = 0; k < kmax; k++){
+		for (int j = 0; j < jmax; j++){
+			for (int i = 0; i < imax; i++){
+				o << i << "," << j << "," << k << ","
+					<< grid2d->vertex(i, j).x() << ","
+					<< grid2d->vertex(i, j).y() << ","
+					<< container->value(grid2d->vertexIndex(i, j)) << endl;
+			}
+		}
+	}
+	if (cellcond != 0){
+		o << "I , J , K , FLAGS" << endl;
+		for (int k = 0; k < kmax - 1; k++){
+			for (int j = 0; j < jmax - 1; j++){
+				for (int i = 0; i < imax - 1; i++){
+					o << i << "," << j << "," << k << ","
+						<< cellcond->value(grid2d->cellIndex(i, j)) << endl;
+				}
+			}
+		}
 	}
 	f.close();
 	return true;
