cba9b535db0f212ff790ef99ac6ff919908c9f1f
zxyecho <58621967+zxyecho@users.noreply.github.com>
2020-01-09 13:50:15 +0900

    Update iricGdNetcdf_zh_CN.ts
    - Replace   <translation type="unfinished"> to </translation>

--- languages/iricGdNetcdf_zh_CN.ts
+++ languages/iricGdNetcdf_zh_CN.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../geodatanetcdf.cpp" line="131"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">错误</translation>
     </message>
     <message>
         <location filename="../geodatanetcdf.cpp" line="131"/>
         <source>Error occured while opening %1.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">打开 %1时发生错误</translation>
     </message>
 </context>
 <context>
@@ -19,12 +19,12 @@
     <message>
         <location filename="../geodatanetcdfcreator.cpp" line="13"/>
         <source>Raster data</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">栅格数据</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfcreator.cpp" line="23"/>
         <source>Raster data%1</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">栅格数据 %1</translation>
     </message>
 </context>
 <context>
@@ -37,17 +37,17 @@
     <message>
         <location filename="../geodatanetcdfgdalimporter.cpp" line="33"/>
         <source>GeoTiff files(*.tif)</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">GeoTiff 文件</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfgdalimporter.cpp" line="49"/>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">警告</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfgdalimporter.cpp" line="49"/>
         <source>GDAL data files can be imported for grid conditions without dimensions.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">可以导入GDAL数据文件以用于没有尺寸的网格条件。</translation>
     </message>
 </context>
 <context>
@@ -60,29 +60,29 @@
     <message>
         <location filename="../geodatanetcdfimporter.cpp" line="47"/>
         <source>NetCDF file (*.nc)</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">NetCDF 文件</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfimporter.cpp" line="112"/>
         <location filename="../geodatanetcdfimporter.cpp" line="162"/>
         <location filename="../geodatanetcdfimporter.cpp" line="325"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">错误</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfimporter.cpp" line="112"/>
         <source>%1 does not have longitude and latitude data.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"> %1 无经纬度数据</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfimporter.cpp" line="162"/>
         <source>%1 does not have variable that can be imported.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"> %1 无可导入变量</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfimporter.cpp" line="325"/>
         <source>Dimension values for %1 mismatch.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">%1 的维度值不匹配</translation>
     </message>
 </context>
 <context>
@@ -90,57 +90,57 @@
     <message>
         <location filename="../geodatanetcdfimporterdateselectdialog.ui" line="14"/>
         <source>Specify Original Date</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">指明原始日期</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfimporterdateselectdialog.ui" line="20"/>
         <source>Automatic recognition of original time failed. Please specify the original date.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">自动识别原始时间失败。 请指定原始日期。</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfimporterdateselectdialog.ui" line="32"/>
         <source>Units:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">单位：</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfimporterdateselectdialog.ui" line="39"/>
         <source>days since 2014-01-01</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">从2012-01-01起的天数</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfimporterdateselectdialog.ui" line="46"/>
         <source>Unit:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">单位：</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfimporterdateselectdialog.ui" line="54"/>
         <source>years</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">年</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfimporterdateselectdialog.ui" line="59"/>
         <source>days</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">日</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfimporterdateselectdialog.ui" line="64"/>
         <source>hours</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">小时</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfimporterdateselectdialog.ui" line="69"/>
         <source>minutes</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">分钟</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfimporterdateselectdialog.ui" line="74"/>
         <source>seconds</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">秒</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfimporterdateselectdialog.ui" line="82"/>
         <source>Original time:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">原始时间：</translation>
     </message>
 </context>
 <context>
@@ -148,17 +148,17 @@
     <message>
         <location filename="../geodatanetcdfimportersettingdialog.ui" line="14"/>
         <source>Dimension Mapping Setting</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">维度地图设置</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfimportersettingdialog.ui" line="22"/>
         <source>Value:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">值：</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfimportersettingdialog.ui" line="34"/>
         <source>Dimensions</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">维度</translation>
     </message>
 </context>
 <context>
@@ -166,42 +166,42 @@
     <message>
         <location filename="../geodatanetcdfxbandimporter.cpp" line="31"/>
         <source>XBand MP RADER data</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">XBand MP RADER 数据</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfxbandimporter.cpp" line="40"/>
         <source>X band MP rader data (*.*)</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">X band MP rader 数据(*.*)</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfxbandimporter.cpp" line="58"/>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">警告</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfxbandimporter.cpp" line="58"/>
         <source>X band MP rader data can be imported for grid conditions with dimension &quot;Time&quot;.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">X波段MP光栅数据可以针对具有时间的网格条件导入</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfxbandimporter.cpp" line="86"/>
         <source>Information</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">信息</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfxbandimporter.cpp" line="86"/>
         <source>%1 files in the folder %2 are imported.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"> 已导入在 %2 文件夹中的 %1 文件</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfxbandimporter.cpp" line="187"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">错误</translation>
     </message>
     <message>
         <location filename="../geodatanetcdfxbandimporter.cpp" line="187"/>
         <source>Dimension values for time mismatch.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">时间的维度值不匹配</translation>
     </message>
 </context>
 </TS>
