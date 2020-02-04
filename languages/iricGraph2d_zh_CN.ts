1622e4a89c7c7508f70f0564297cbcac03b3961e
zxyecho <58621967+zxyecho@users.noreply.github.com>
2020-02-04 22:11:47 +0900

    Update iricGraph2d_zh_CN.ts

--- languages/iricGraph2d_zh_CN.ts
+++ languages/iricGraph2d_zh_CN.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../graph2dwindowdatamodel.cpp" line="291"/>
         <source>Title Setting</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">标题设置</translation>
     </message>
     <message>
         <location filename="../graph2dwindowdatamodel.cpp" line="291"/>
         <source>Please input the title.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">请输入标题</translation>
     </message>
 </context>
 <context>
@@ -19,7 +19,7 @@
     <message>
         <location filename="../datamodel/graph2dwindowmarkergroupdataitem.cpp" line="22"/>
         <source>Markers</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">标记</translation>
     </message>
 </context>
 <context>
@@ -27,122 +27,122 @@
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="14"/>
         <source>Marker Setting</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">标记设置</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="36"/>
         <source>Orientation</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">方向</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="42"/>
         <source>X-axis</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">X轴</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="49"/>
         <source>Y-axis</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Y轴</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="62"/>
         <source>Axis</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">轴</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="68"/>
         <source>Left</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">左</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="78"/>
         <source>Right</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">右</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="105"/>
         <source>Marker Value</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">标记值</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="111"/>
         <source>Value:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">数值：</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="141"/>
         <source>Label</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">标签</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="147"/>
         <source>Auto</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">自动</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="159"/>
         <source>Custom</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">自定义</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="177"/>
         <source>Position</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">位置</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="183"/>
         <source>Center</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">中心</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="193"/>
         <source>Above Line</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">线以上</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="238"/>
         <source>Style</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">样式</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="244"/>
         <source>Color: </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">颜色</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="258"/>
         <source>Show line</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">显示线</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="269"/>
         <source>Solid Line</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">实线</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="274"/>
         <source>Dashed Line</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">虚线</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="279"/>
         <source>Dotted Line</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">点线</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="284"/>
         <source>Dots and Dashes</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">点虚线</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="325"/>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">添加</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.ui" line="332"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">删除</translation>
     </message>
     <message>
         <location filename="../graph2dwindowmarkersettingdialog.cpp" line="103"/>
@@ -155,38 +155,38 @@
     <message>
         <location filename="../graph2dwindowverticalaxissettingdialog.ui" line="14"/>
         <source>Vertical Axis Setting</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">纵坐标设定</translation>
     </message>
     <message>
         <location filename="../graph2dwindowverticalaxissettingdialog.ui" line="24"/>
         <source>Value Range</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">数值区间</translation>
     </message>
     <message>
         <location filename="../graph2dwindowverticalaxissettingdialog.ui" line="30"/>
         <location filename="../graph2dwindowverticalaxissettingdialog.ui" line="85"/>
         <source>Auto</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">自动</translation>
     </message>
     <message>
         <location filename="../graph2dwindowverticalaxissettingdialog.ui" line="39"/>
         <source>Max</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">最大</translation>
     </message>
     <message>
         <location filename="../graph2dwindowverticalaxissettingdialog.ui" line="49"/>
         <source>Min</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">最小</translation>
     </message>
     <message>
         <location filename="../graph2dwindowverticalaxissettingdialog.ui" line="79"/>
         <source>Label</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">标记</translation>
     </message>
     <message>
         <location filename="../graph2dwindowverticalaxissettingdialog.ui" line="94"/>
         <source>Custom</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">自定义</translation>
     </message>
 </context>
 <context>
@@ -194,17 +194,17 @@
     <message>
         <location filename="../private/graph2dwindowdataitem_standarditemchangecommand.cpp" line="4"/>
         <source>Object Browser Item Change</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">对象浏览器项目更改</translation>
     </message>
     <message>
         <location filename="../graph2dwindowrootdataitem.cpp" line="81"/>
         <source>Move up item</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">上移项目</translation>
     </message>
     <message>
         <location filename="../graph2dwindowrootdataitem.cpp" line="115"/>
         <source>Move down item</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">下移项目</translation>
     </message>
 </context>
 </TS>
