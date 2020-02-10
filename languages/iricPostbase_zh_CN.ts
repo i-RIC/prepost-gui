cbb94e156e6e47025462c29aa8b66b152dd374e9
zxyecho <58621967+zxyecho@users.noreply.github.com>
2020-02-10 11:28:27 +0900

    Update iricPostbase_zh_CN.ts

--- languages/iricPostbase_zh_CN.ts
+++ languages/iricPostbase_zh_CN.ts
@@ -6,37 +6,37 @@
     <message>
         <location filename="../postallzoneselectingdialog.ui" line="14"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">对话框</translation>
     </message>
     <message>
         <location filename="../postallzoneselectingdialog.ui" line="22"/>
         <source>Dimension:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">维度：</translation>
     </message>
     <message>
         <location filename="../postallzoneselectingdialog.ui" line="49"/>
         <source>Grid Type:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">网格种类：</translation>
     </message>
     <message>
         <location filename="../postallzoneselectingdialog.ui" line="78"/>
         <source>&amp;Region:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&amp;区域：</translation>
     </message>
     <message>
         <location filename="../postallzoneselectingdialog.cpp" line="35"/>
         <source>One dimensional data</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">1d数据</translation>
     </message>
     <message>
         <location filename="../postallzoneselectingdialog.cpp" line="46"/>
         <source>Two dimensional data</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">2d数据</translation>
     </message>
     <message>
         <location filename="../postallzoneselectingdialog.cpp" line="57"/>
         <source>Three dimensional data</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">3d数据</translation>
     </message>
 </context>
 <context>
@@ -44,17 +44,17 @@
     <message>
         <location filename="../particle/postparticlebasicsettingdialog.ui" line="14"/>
         <source>Particles Setting</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">颗粒设置</translation>
     </message>
     <message>
         <location filename="../particle/postparticlebasicsettingdialog.ui" line="22"/>
         <source>Color:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">颜色：</translation>
     </message>
     <message>
         <location filename="../particle/postparticlebasicsettingdialog.ui" line="53"/>
         <source>Size:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">大小：</translation>
     </message>
 </context>
 <context>
@@ -62,32 +62,32 @@
     <message>
         <location filename="../particle/postparticlescalarpropertydialog.ui" line="14"/>
         <source>Particle Scalar Setting</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">粒子标量设置</translation>
     </message>
     <message>
         <location filename="../particle/postparticlescalarpropertydialog.ui" line="26"/>
         <source>Custom Color</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">自定义颜色</translation>
     </message>
     <message>
         <location filename="../particle/postparticlescalarpropertydialog.ui" line="64"/>
         <source>Color by attribute value</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">根据属性值上色</translation>
     </message>
     <message>
         <location filename="../particle/postparticlescalarpropertydialog.ui" line="89"/>
         <source>Attribute:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">属性：</translation>
     </message>
     <message>
         <location filename="../particle/postparticlescalarpropertydialog.ui" line="127"/>
         <source>Size:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">大小：</translation>
     </message>
     <message>
         <location filename="../particle/postparticlescalarpropertydialog.ui" line="171"/>
         <source>&amp;Color Bar Setting</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&amp;颜色条设置</translation>
     </message>
 </context>
 <context>
@@ -95,27 +95,27 @@
     <message>
         <location filename="../particle/postparticlevectorpropertydialog.ui" line="17"/>
         <source>Arrow Setting</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">箭头设置</translation>
     </message>
     <message>
         <location filename="../particle/postparticlevectorpropertydialog.ui" line="31"/>
         <source>Attribute value:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">属性值：</translation>
     </message>
     <message>
         <location filename="../particle/postparticlevectorpropertydialog.ui" line="56"/>
         <source>Length</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">长度</translation>
     </message>
     <message>
         <location filename="../particle/postparticlevectorpropertydialog.ui" line="62"/>
         <source>Auto</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">自动</translation>
     </message>
     <message>
         <location filename="../particle/postparticlevectorpropertydialog.ui" line="76"/>
         <source>Standard value:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">标准值：</translation>
     </message>
     <message>
         <location filename="../particle/postparticlevectorpropertydialog.ui" line="105"/>
@@ -126,67 +126,67 @@
     <message>
         <location filename="../particle/postparticlevectorpropertydialog.ui" line="112"/>
         <source>Length on screen:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">屏幕长度：</translation>
     </message>
     <message>
         <location filename="../particle/postparticlevectorpropertydialog.ui" line="132"/>
         <source>[pixel]</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">[像素]</translation>
     </message>
     <message>
         <location filename="../particle/postparticlevectorpropertydialog.ui" line="139"/>
         <source>Minimum value to draw:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">绘制最小值：</translation>
     </message>
     <message>
         <location filename="../particle/postparticlevectorpropertydialog.ui" line="195"/>
         <source>Sampling</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">取样</translation>
     </message>
     <message>
         <location filename="../particle/postparticlevectorpropertydialog.ui" line="203"/>
         <source>All particles</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">所有粒子</translation>
     </message>
     <message>
         <location filename="../particle/postparticlevectorpropertydialog.ui" line="213"/>
         <source>Sampling rate:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">样本率</translation>
     </message>
     <message>
         <location filename="../particle/postparticlevectorpropertydialog.ui" line="233"/>
         <source>Specify number of particles</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">指明粒子数量</translation>
     </message>
     <message>
         <location filename="../particle/postparticlevectorpropertydialog.ui" line="271"/>
         <source>Color</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">颜色</translation>
     </message>
     <message>
         <location filename="../particle/postparticlevectorpropertydialog.ui" line="283"/>
         <source>Custom color</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">自定义颜色</translation>
     </message>
     <message>
         <location filename="../particle/postparticlevectorpropertydialog.ui" line="313"/>
         <source>By scalar value</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">根据标量值</translation>
     </message>
     <message>
         <location filename="../particle/postparticlevectorpropertydialog.ui" line="336"/>
         <source>Arrow Shape</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">箭头形状</translation>
     </message>
     <message>
         <location filename="../particle/postparticlevectorpropertydialog.ui" line="344"/>
         <source>Arrow size:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">箭头大小：</translation>
     </message>
     <message>
         <location filename="../particle/postparticlevectorpropertydialog.ui" line="361"/>
         <source>Line width:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">行宽：</translation>
     </message>
 </context>
 <context>
@@ -194,17 +194,17 @@
     <message>
         <location filename="../polydata/postpolydatabasicsettingdialog.ui" line="14"/>
         <source>Polygon Setting</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">多边形设置</translation>
     </message>
     <message>
         <location filename="../polydata/postpolydatabasicsettingdialog.ui" line="22"/>
         <source>Color:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">颜色：</translation>
     </message>
     <message>
         <location filename="../polydata/postpolydatabasicsettingdialog.ui" line="53"/>
         <source>Line Width:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">行宽：</translation>
     </message>
 </context>
 <context>
@@ -212,32 +212,32 @@
     <message>
         <location filename="../polydata/postpolydatascalarpropertydialog.ui" line="14"/>
         <source>Polygon Setting</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">多边形设置</translation>
     </message>
     <message>
         <location filename="../polydata/postpolydatascalarpropertydialog.ui" line="26"/>
         <source>Custom Color</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">自定义颜色</translation>
     </message>
     <message>
         <location filename="../polydata/postpolydatascalarpropertydialog.ui" line="64"/>
         <source>Color by attribute value</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">按属性值颜色</translation>
     </message>
     <message>
         <location filename="../polydata/postpolydatascalarpropertydialog.ui" line="89"/>
         <source>Attribute:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">属性：</translation>
     </message>
     <message>
         <location filename="../polydata/postpolydatascalarpropertydialog.ui" line="127"/>
         <source>Line Width</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">行宽</translation>
     </message>
     <message>
         <location filename="../polydata/postpolydatascalarpropertydialog.ui" line="171"/>
         <source>&amp;Color Bar Setting</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&amp;颜色条设置</translation>
     </message>
 </context>
 <context>
@@ -245,12 +245,12 @@
     <message>
         <location filename="../time/posttimedataitem.cpp" line="67"/>
         <source>Time</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">时间</translation>
     </message>
     <message>
         <location filename="../time/private/posttimedataitem_setsettingcommand.cpp" line="4"/>
         <source>Edit time setting</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">编辑时间设置</translation>
     </message>
 </context>
 <context>
@@ -258,17 +258,17 @@
     <message>
         <location filename="../time/posttimeeditdialog.ui" line="14"/>
         <source>Time Setting</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">时间设置</translation>
     </message>
     <message>
         <location filename="../time/posttimeeditdialog.ui" line="20"/>
         <source>Use project setting</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">使用项目设定</translation>
     </message>
     <message>
         <location filename="../time/posttimeeditdialog.ui" line="30"/>
         <source>Time Format</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">时间格式</translation>
     </message>
 </context>
 <context>
@@ -276,7 +276,7 @@
     <message>
         <location filename="../title/posttitledataitem.cpp" line="20"/>
         <source>Title</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">标题</translation>
     </message>
 </context>
 <context>
@@ -284,17 +284,17 @@
     <message>
         <location filename="../title/posttitleeditdialog.ui" line="14"/>
         <source>Title Setting</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">标题设置</translation>
     </message>
     <message>
         <location filename="../title/posttitleeditdialog.ui" line="22"/>
         <source>Title:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">标题：</translation>
     </message>
     <message>
         <location filename="../title/posttitleeditdialog.ui" line="56"/>
         <source>Note: Only English Characters can be used for title.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">注意：标题只能使用英文字符</translation>
     </message>
 </context>
 </TS>
