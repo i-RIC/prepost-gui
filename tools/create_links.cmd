8df35ff9dd348ab72e20388e0ab9429903a2066f
Charlton, Scott R <charlton@usgs.gov>
2020-10-26 16:51:43 -0600

    added create_links.cmd to tools folder

---
+++ tools/create_links.cmd
@@ -0,0 +1,6 @@
+:: Create symbolic links to installed iRIC_dev solvers/plugins/gridcreators
+::
+mklink /d ..\apps\solvers              %USERPROFILE%\iRIC_dev\solvers
+mklink /d ..\apps\gridexporter_plugins %USERPROFILE%\iRIC_dev\gridexporter_plugins
+mklink /d ..\apps\gridimporter_plugins %USERPROFILE%\iRIC_dev\gridimporter_plugins
+mklink /d ..\apps\gridcreators         %USERPROFILE%\iRIC_dev\gridcreators
