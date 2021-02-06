2d5022b4f31c11cb89ad6c2f91267b0e8d3f5a07
Keisuke Inoue <kskinoue@gmail.com>
2021-02-06 19:32:20 +0900

    Fixed bug that exported image is upside down.

--- libs/geodata/netcdf/geodatanetcdfgdalintegerexporter.cpp
+++ libs/geodata/netcdf/geodatanetcdfgdalintegerexporter.cpp
@@ -14,11 +14,19 @@ void GeoDataNetcdfGdalIntegerExporter::copyData(GeoDataNetcdf* netcdf, int ncid,
 {
 	size_t buffersize = *(ends) * *(ends + 1);
 	std::vector<int> buffer(buffersize);
+	std::vector<int> buffer2(buffersize);
 
 	auto netcdfi = dynamic_cast<GeoDataNetcdfInteger*> (netcdf);
 
 	band->SetNoDataValue(netcdfi->missingValue());
 
 	int ret = nc_get_vara_int(ncid, varid, starts, ends, buffer.data());
+
+	for (int j = 0; j < netcdf->ySize(); ++j) {
+		for (int i = 0; i < netcdf->xSize(); ++i) {
+			buffer2[(netcdf->ySize() - 1 - j) * netcdf->xSize() + i] = buffer[j * netcdf->xSize() + i];
+		}
+	}
+
 	GDALRasterIO(band, GF_Write, 0, 0, netcdf->xSize(), netcdf->ySize(), buffer.data(), netcdf->xSize(), netcdf->ySize(), GDT_Int32, 0, 0);
 }
--- libs/geodata/netcdf/geodatanetcdfgdalrealexporter.cpp
+++ libs/geodata/netcdf/geodatanetcdfgdalrealexporter.cpp
@@ -19,11 +19,19 @@ void GeoDataNetcdfGdalRealExporter::copyData(GeoDataNetcdf* netcdf, int ncid, in
 {
 	size_t buffersize = *(ends) * *(ends + 1);
 	std::vector<double> buffer(buffersize);
+	std::vector<double> buffer2(buffersize);
 
 	auto netcdfr = dynamic_cast<GeoDataNetcdfReal*> (netcdf);
 
 	band->SetNoDataValue(netcdfr->missingValue());
 
 	int ret = nc_get_vara_double(ncid, varid, starts, ends, buffer.data());
-	GDALRasterIO(band, GF_Write, 0, 0, netcdf->xSize(), netcdf->ySize(), buffer.data(), netcdf->xSize(), netcdf->ySize(), GDT_Float64, 0, 0);
+
+	for (int j = 0; j < netcdf->ySize(); ++j) {
+		for (int i = 0; i < netcdf->xSize(); ++i) {
+			buffer2[(netcdf->ySize() - 1 - j) * netcdf->xSize() + i] = buffer[j * netcdf->xSize() + i];
+		}
+	}
+
+	GDALRasterIO(band, GF_Write, 0, 0, netcdf->xSize(), netcdf->ySize(), buffer2.data(), netcdf->xSize(), netcdf->ySize(), GDT_Float64, 0, 0);
 }
