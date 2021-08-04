9eeb5543cf12cbad95ee34e45f8332ed64ff8f33
Charlton, Scott R <charlton@usgs.gov>
2021-08-03 18:53:11 -0600

    updated lib paths

--- tools/copydllsExt-2019.py
+++ tools/copydllsExt-2019.py
@@ -21,7 +21,8 @@ config.read(configFile)
 # check config.
 sections = ["release", "debug"]
 # options  = ["cgnslib", "expat", "gdal", "geos", "hdf5", "iriclib", "netcdf", "openssl", "proj", "qwt", "shapelib", "szip", "udunits", "vtk", "yaml-cpp", "zlib"]
-options  = ["cgnslib", "expat", "gdal", "geos", "hdf5", "iriclib", "libpng", "netcdf", "openssl", "proj", "qwt", "shapelib", "szip", "tiff", "udunits", "vtk", "yaml-cpp", "zlib"]
+## options  = ["expat", "gdal", "geos", "hdf5", "iriclib", "libpng", "netcdf", "openssl", "poco", "proj", "qwt", "shapelib", "szip", "tiff", "udunits", "vtk", "yaml-cpp", "zlib"]
+options  = ["expat", "gdal", "geos", "hdf5", "iriclib", "libpng", "netcdf", "openssl", "poco", "proj", "qwt", "shapelib", "sqlite3", "tiff", "udunits", "vtk", "yaml-cpp"]
 for section in sections:
   if (section in config.sections()):
     for option in options:
@@ -35,7 +36,7 @@ dirList = {}
 for section in sections:
   dirList[section] = []
   for option in options:
-    if (option == "qwt"):
+    if ((option == "hdf5") or (option == "iriclib") or (option == "poco")  or (option == "qwt") ):
       continue
     else:
       dirList[section].append(config.get(section, option))
@@ -46,10 +47,22 @@ exList = ["msvcp120.dll", "msvcr120.dll"]
 # dll files that are not included in dirList:
 fileList = {}
 fileList["release"] = [
-  str(Path(config.get("release", "qwt"), "qwt.dll")),
+  str(Path(config.get("release", "iriclib"), "iriclib.dll")),
+  str(Path(config.get("release", "hdf5"),    "hdf5.dll")),
+  str(Path(config.get("release", "hdf5"),    "hdf5_hl.dll")),
+  str(Path(config.get("release", "hdf5"),    "szip.dll")),
+  str(Path(config.get("release", "hdf5"),    "zlib.dll")),
+  str(Path(config.get("release", "poco"),    "PocoFoundation.dll")),
+  str(Path(config.get("release", "qwt"),     "qwt.dll"))
 ]
 fileList["debug"] = [
-  str(Path(config.get("debug",   "qwt"), "qwtd.dll")),
+  str(Path(config.get("debug",  "iriclib"),  "iriclibd.dll")),
+  str(Path(config.get("debug",  "hdf5"),     "hdf5_D.dll")),
+  str(Path(config.get("debug",  "hdf5"),     "hdf5_hl_D.dll")),
+  str(Path(config.get("debug",  "hdf5"),     "szip_D.dll")),
+  str(Path(config.get("debug",  "hdf5"),     "zlib_D.dll")),
+  str(Path(config.get("debug",  "poco"),     "PocoFoundationd.dll")),
+  str(Path(config.get("debug",  "qwt"),      "qwtd.dll")),
 ]
 
 # copy dll
