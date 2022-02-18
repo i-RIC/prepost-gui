d097f591cbe8369abc6ae28a6978a435acc6124c
Charlton, Scott R <charlton@usgs.gov>
2022-02-17 23:35:45 -0700

    more testing

---
+++ input/iricDistancemeasure_it_IT.ts
@@ -0,0 +1,157 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>DistanceMeasureDataItem</name>
+    <message>
+        <source>Property</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Redefine</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Define Distance Measure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Move Distance Measure Point</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Redefine Distance Measure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Edit Distance Measure Property</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Translate Distance Measure</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DistanceMeasureGroupDataItem</name>
+    <message>
+        <source>&amp;Add Measure...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Distance Measures</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Measure%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Delete &amp;Selected...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Delete &amp;All...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>There is no distance measure.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Delete selected distance measures</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Are you sure you want to delete all distance measures?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DistanceMeasurePropertyDialog</name>
+    <message>
+        <source>Distance Measure Property</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Start Point:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>, </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>End Point:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Label</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show Label</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Content</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Auto</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Custom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Center-Top</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Middle-Left</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Middle-Right</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Center-Bottom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Font Setting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show End Markers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Marker Size:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Color:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
