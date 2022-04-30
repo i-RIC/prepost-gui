33c9ca8194dfae7564d6c97a2f072163624eaa96
Keisuke Inoue <kskinoue@gmail.com>
2022-04-30 17:43:26 +0900

    Added const keyword.

--- libs/guibase/polyline/polylineutil.h
+++ libs/guibase/polyline/polylineutil.h
@@ -14,8 +14,8 @@ class GUIBASEDLL_EXPORT PolyLineUtil
 {
 public:
 	static std::vector<QPointF> buildSplinePoints(vtkPoints* points, int divNum);
-	static double length(std::vector<QPointF>& polyLine);
-	static QRectF boundingRect(std::vector<QPointF>& polyLine);
+	static double length(const std::vector<QPointF>& polyLine);
+	static QRectF boundingRect(const std::vector<QPointF>& polyLine);
 
 private:
 	PolyLineUtil();
