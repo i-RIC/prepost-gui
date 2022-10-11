86c8748cb33d8541dfa19e2dea07863c4a5a64fa
Keisuke Inoue <kskinoue@gmail.com>
2022-10-11 19:01:26 +0900

    Close #1070
    Bug fix: Importing project from *.ipro fails.

--- libs/pre/gridimporter/projectgridimporter.h
+++ libs/pre/gridimporter/projectgridimporter.h
@@ -5,6 +5,7 @@
 #include <QObject>
 
 class CgnsGridImporter;
+class PreProcessorGridDataItem;
 
 class ProjectGridImporter : public GridInternalImporter
 {
@@ -14,13 +15,20 @@ class ProjectGridImporter : public GridInternalImporter
 public:
 	ProjectGridImporter();
 	virtual ~ProjectGridImporter();
+
+	void setGridDataItem(PreProcessorGridDataItem* gridDataItem);
+
 	QString caption() const override;
 	QStringList fileDialogFilters() const override;
+
 	/// Import grid data from external file.
 	bool import(Grid* grid, const QString& filename, const QString& selectedFilter, QWidget* parent) override;
 
 protected:
 	virtual CgnsGridImporter* getCgnsImporter() = 0;
+
+private:
+	PreProcessorGridDataItem* m_gridDataItem;
 };
 
 #endif // PROJECTGRIDIMPORTER_H
