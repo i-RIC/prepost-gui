cbe998201150d38d2379e6e6f70691916035cb37
Keisuke Inoue <kskinoue@gmail.com>
2022-11-21 21:56:45 +0900

    Preference dialog updated

--- libs/gui/pref/preferencepagefontdefault.cpp
+++ libs/gui/pref/preferencepagefontdefault.cpp
@@ -13,6 +13,8 @@ PreferencePageFontDefault::PreferencePageFontDefault(QWidget* parent) :
 	ui->setupUi(this);
 	ui->bcNamesWidget->hideColor();
 	ui->arrowWidget->hideColor();
+	ui->colorBarTitleWidget->hideSize();
+	ui->colorBarLabelWidget->hideSize();
 
 	QSettings settings;
 
@@ -29,21 +31,41 @@ PreferencePageFontDefault::PreferencePageFontDefault(QWidget* parent) :
 	*/
 
 	// tested
+
 	vtkTextPropertySettingContainer s_gridIndices;
-	s_gridIndices.loadSetting(m_settings, "fontsetting/gridindices");
+	s_gridIndices.loadSetting(settings, "fontsetting/gridindices");
 	s_gridIndices.fontSize = 15;
 	ui->gridShapeWidget->setSetting(s_gridIndices);
+
+	vtkTextPropertySettingContainer s_colorBarTitle;
+	s_colorBarTitle.loadSetting(settings, "fontsetting/colorbar_title");
+	ui->colorBarTitleWidget->setSetting(s_colorBarTitle);
+
+	vtkTextPropertySettingContainer s_colorBarLabel;
+	s_colorBarLabel.loadSetting(settings, "fontsetting/colorbar_label");
+	ui->colorBarLabelWidget->setSetting(s_colorBarLabel);
+
+	vtkTextPropertySettingContainer s_arrows;
+	s_arrows.fontSize = 10;
+	s_arrows.loadSetting(settings, "fontsetting/arrow");
+	ui->arrowWidget->setSetting(s_arrows);
+
+	vtkTextPropertySettingContainer s_gridCreatingCondition;
+	s_gridCreatingCondition.fontSize = 17;
+	s_gridCreatingCondition.isBold = true;
+	s_gridCreatingCondition.loadSetting(settings, "fontsetting/gridcreatingcondition");
+	ui->gridCreatingConditionWidget->setSetting(s_gridCreatingCondition);
+
+	vtkTextPropertySettingContainer s_riverSurveyNames;
+	s_riverSurveyNames.fontSize = 17;
+	s_riverSurveyNames.loadSetting(settings, "fontsetting/riversurvey_names");
+	ui->riverSurveyDataWidget->setSetting(s_riverSurveyNames);
 
 	vtkTextPropertySettingContainer s_bcNames;
 	s_bcNames.fontFamily = vtkTextPropertySettingContainer::ffArial;
 	s_bcNames.fontSize = 14;
 	s_bcNames.loadSetting(settings, "fontsetting/bc");
 	ui->bcNamesWidget->setSetting(s_bcNames);
-
-	vtkTextPropertySettingContainer s_arrows;
-	s_arrows.fontSize = 10;
-	s_arrows.loadSetting(m_settings, "fontsetting/arrow");
-	ui->arrowWidget->setSetting(s_arrows);
 }
 
 PreferencePageFontDefault::~PreferencePageFontDefault()
@@ -62,9 +84,21 @@ void PreferencePageFontDefault::update()
 	s = ui->gridShapeWidget->setting();
 	s.saveSetting(&settings, "fontsetting/gridindices");
 
-	s = ui->bcNamesWidget->setting();
-	s.saveSetting(&settings, "fontsetting/bc");
+	s = ui->colorBarTitleWidget->setting();
+	s.saveSetting(&settings, "fontsetting/colorbar_title");
+
+	s = ui->colorBarLabelWidget->setting();
+	s.saveSetting(&settings, "fontsetting/colorbar_label");
 
 	s = ui->arrowWidget->setting();
 	s.saveSetting(&settings, "fontsetting/arrow");
+
+	s = ui->gridCreatingConditionWidget->setting();
+	s.saveSetting(&settings, "fontsetting/gridcreatingcondition");
+
+	s = ui->riverSurveyDataWidget->setting();
+	s.saveSetting(&settings, "fontsetting/riversurvey_names");
+
+	s = ui->bcNamesWidget->setting();
+	s.saveSetting(&settings, "fontsetting/bc");
 }
