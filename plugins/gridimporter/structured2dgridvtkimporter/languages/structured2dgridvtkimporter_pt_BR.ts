5c89999536f4659f0dd78dc8d8a7096696d0c9d5
iric.appveyor <iric.appveyor@gmail.com>
2022-11-28 01:09:20 +0000

    [skip ci] translate_ts.py by iric.appveyor develop_v4-921

--- plugins/gridimporter/structured2dgridvtkimporter/languages/structured2dgridvtkimporter_pt_BR.ts
+++ plugins/gridimporter/structured2dgridvtkimporter/languages/structured2dgridvtkimporter_pt_BR.ts
@@ -1,27 +1,25 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
 <TS version="2.1" language="pt_BR">
 <context>
     <name>Structured2dGridVtkImporter</name>
     <message>
-        <location filename="../structured2dgridvtkimporter.cpp" line="24"/>
+        <location filename="../structured2dgridvtkimporter.cpp" line="24" />
         <source>VTK files (*.vtk)</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished" />
     </message>
     <message>
-        <location filename="../structured2dgridvtkimporter.cpp" line="30"/>
+        <location filename="../structured2dgridvtkimporter.cpp" line="30" />
         <source>VTK file</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished" />
     </message>
     <message>
-        <location filename="../structured2dgridvtkimporter.cpp" line="45"/>
+        <location filename="../structured2dgridvtkimporter.cpp" line="45" />
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished" />
     </message>
     <message>
-        <location filename="../structured2dgridvtkimporter.cpp" line="45"/>
+        <location filename="../structured2dgridvtkimporter.cpp" line="45" />
         <source>Copying %1 to %2 failed.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished" />
     </message>
 </context>
 </TS>
--- plugins/gridimporter/unstructured2dgridvtkimporter/languages/unstructured2dgridvtkimporter_ja_JP.ts
+++ plugins/gridimporter/unstructured2dgridvtkimporter/languages/unstructured2dgridvtkimporter_ja_JP.ts
@@ -1,25 +1,23 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
 <TS version="2.1" language="ja_JP">
 <context>
     <name>Unstructured2dGridVtkImporter</name>
     <message>
-        <location filename="../unstructured2dgridvtkimporter.cpp" line="24"/>
+        <location filename="../unstructured2dgridvtkimporter.cpp" line="24" />
         <source>VTK files (*.vtk)</source>
         <translation>VTK ファイル (*.vtk)</translation>
     </message>
     <message>
-        <location filename="../unstructured2dgridvtkimporter.cpp" line="30"/>
+        <location filename="../unstructured2dgridvtkimporter.cpp" line="30" />
         <source>VTK file</source>
         <translation>VTKファイル</translation>
     </message>
     <message>
-        <location filename="../unstructured2dgridvtkimporter.cpp" line="45"/>
+        <location filename="../unstructured2dgridvtkimporter.cpp" line="45" />
         <source>Error</source>
         <translation>エラー</translation>
     </message>
     <message>
-        <location filename="../unstructured2dgridvtkimporter.cpp" line="45"/>
+        <location filename="../unstructured2dgridvtkimporter.cpp" line="45" />
         <source>Copying %1 to %2 failed.</source>
         <translation>%1 を %2 にコピーできませんでした。</translation>
     </message>
