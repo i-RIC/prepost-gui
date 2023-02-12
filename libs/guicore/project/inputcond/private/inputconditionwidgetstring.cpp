9a3d34c70466a4c2907affd1cb57515466e96588
Keisuke Inoue <kskinoue@gmail.com>
2023-02-12 10:05:29 +0900

    Close #1107
    Add function to import source text into Textarea on input condition dialog

--- libs/guicore/project/inputcond/private/inputconditionwidgetstring.cpp
+++ libs/guicore/project/inputcond/private/inputconditionwidgetstring.cpp
@@ -5,20 +5,26 @@
 
 #include <guibase/widget/asciionlylineedit.h>
 #include <guibase/widget/asciionlytextedit.h>
+#include <misc/informationdialog.h>
 
 #include <QHBoxLayout>
 #include <QMessageBox>
 #include <QTextCodec>
+#include <QVBoxLayout>
 
-InputConditionWidgetString::InputConditionWidgetString(QDomNode defnode, const SolverDefinitionTranslator& /*t*/, InputConditionContainerString* cont) :
+InputConditionWidgetString::InputConditionWidgetString(QDomNode defnode, const SolverDefinitionTranslator& t, InputConditionContainerString* cont, const QString& folderName) :
 	InputConditionWidget(defnode),
-	m_container {cont}
+	m_container {cont},
+	m_sourceComboBox {nullptr}
 {
-	QHBoxLayout* layout = new QHBoxLayout(this);
+	auto layout = new QVBoxLayout(this);
 	layout->setMargin(InputConditionWidget::margin);
 	if (defnode.toElement().attribute("multiline") == "true") {
 		AsciiOnlyTextEdit* te = new AsciiOnlyTextEdit(this);
 		connect(te, SIGNAL(textChanged()), this, SLOT(handleEditingFinished()));
+
+		setupComboBoxIfNeeded(defnode, t, cont, folderName, layout);
+
 		layout->addWidget(te, 1);
 		m_textEdit = te;
 	}	else {
@@ -52,4 +58,61 @@ void InputConditionWidgetString::setValue(const QString& newvalue)
 void InputConditionWidgetString::handleEditingFinished()
 {
 	m_container->setValue(m_textEdit->text());
+}
+
+void InputConditionWidgetString::setupComboBoxIfNeeded(QDomNode defnode, const SolverDefinitionTranslator& t, InputConditionContainerString* cont, const QString& folderName, QLayout* l)
+{
+	QDir folder(folderName);
+
+	std::vector<QString> captions;
+	const auto cn = defnode.childNodes();
+	for (int i = 0; i < cn.size(); ++i) {
+		auto child = cn.at(i);
+		if (child.nodeName() != "Source") {continue;}
+		auto elem = child.toElement();
+
+		captions.push_back(t.translate(elem.attribute("caption")));
+		auto fname = folder.absoluteFilePath(elem.attribute("filename"));
+		m_sourceFileNames.push_back(fname);
+		if (elem.attribute("default") == "true") {
+			auto utf8 = QTextCodec::codecForName("utf-8");
+			QFile f(fname);
+			f.open(QFile::ReadOnly);
+			auto content = utf8->toUnicode(f.readAll());
+			cont->setDefaultValue(content);
+			cont->setValue(content);
+		}
+	}
+
+	if (captions.size() > 0) {
+		m_sourceComboBox = new QComboBox(this);
+		m_sourceComboBox->addItem(tr("(Select template to import)"));
+
+		for (int i = 0; i < captions.size(); ++i) {
+			m_sourceComboBox->addItem(captions.at(i));
+		}
+		connect<void (QComboBox::*)(int)>(m_sourceComboBox, &QComboBox::currentIndexChanged, this, &InputConditionWidgetString::applyTemplate);
+
+		auto l2 = new QHBoxLayout(this);
+		l2->addWidget(m_sourceComboBox);
+		l2->addStretch(1);
+		l->addItem(l2);
+	}
+}
+
+void InputConditionWidgetString::applyTemplate(int index)
+{
+	int ret = QMessageBox::warning(this, tr("Warning"), tr("The content in the text area is overwritten with the template"), QMessageBox::Ok | QMessageBox::Cancel, QMessageBox::Cancel);
+	if (ret == QMessageBox::Ok) {
+		auto fname = m_sourceFileNames.at(index - 1);
+		auto utf8 = QTextCodec::codecForName("utf-8");
+		QFile f(fname);
+		f.open(QFile::ReadOnly);
+		auto content = utf8->toUnicode(f.readAll());
+		m_textEdit->setText(content);
+	}
+
+	m_sourceComboBox->blockSignals(true);
+	m_sourceComboBox->setCurrentIndex(0);
+	m_sourceComboBox->blockSignals(false);
 }
