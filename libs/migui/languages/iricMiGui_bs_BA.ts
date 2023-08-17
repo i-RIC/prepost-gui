d04043d2bffdd7a16684d533f0fd525dc6039b3e
Keisuke Inoue <kskinoue@gmail.com>
2023-08-17 23:22:50 +0900

    iRIC-MI GUI dictionary prepared.

---
+++ libs/migui/languages/iricMiGui_bs_BA.ts
@@ -0,0 +1,285 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="bs_BA">
+<context>
+    <name>ConnectionAddDialog</name>
+    <message>
+        <source>Add Connection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Output</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Model:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Value:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Input</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ExistingModelAddDialog</name>
+    <message>
+        <source>Add Existing Models</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Models</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Folder name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Solver</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Model</name>
+    <message>
+        <source>Output time interval</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ModelAddDialog</name>
+    <message>
+        <source>Add Model</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Folder Name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please input the folder name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Model</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please input folder name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You can use only ASCII characters.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Folder &quot;%1&quot; already exists.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ModelDeleteDialog</name>
+    <message>
+        <source>Delete Model</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Target Folder:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Keep the content in the folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Delete all files in the folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>iRICMIMainWindow</name>
+    <message>
+        <source>iRIC-MI 0.0.1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Models</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Add...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add &amp;Existing...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Remove...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Connections</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Visualize...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;New Project...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ctrl+N</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Open...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ctrl+O</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ctrl+S</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Property...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Recent Projects</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;About...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Reload</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Saving %1 failed. Please make sure other programs are opening the file, and try again.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Project successfully saved to %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error occured while preparing data for model in &quot;%1&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No existing project folder found in &quot;%1&quot;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Are you sure you want to delete this connection?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select folder to save project</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 already exists. Please select another folder.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select project file to open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>iRIC-MI project file (iricmi_project.xml)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The project is modified. Do you want to save?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Saving &quot;%1&quot; failed. Please make sure other program is opening the file, and try again.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>iRICMIProject</name>
+    <message>
+        <source>Folder name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Solver</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Output model</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Output value</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Input model</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Input value</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ libs/migui/languages/iricMiGui_ja_JP.ts
@@ -0,0 +1,285 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>ConnectionAddDialog</name>
+    <message>
+        <source>Add Connection</source>
+        <translation>接続の追加</translation>
+    </message>
+    <message>
+        <source>Output</source>
+        <translation>出力</translation>
+    </message>
+    <message>
+        <source>Model:</source>
+        <translation>モデル:</translation>
+    </message>
+    <message>
+        <source>Value:</source>
+        <translation>値:</translation>
+    </message>
+    <message>
+        <source>Input</source>
+        <translation>入力</translation>
+    </message>
+</context>
+<context>
+    <name>ExistingModelAddDialog</name>
+    <message>
+        <source>Add Existing Models</source>
+        <translation>既存モデルの追加</translation>
+    </message>
+    <message>
+        <source>Models</source>
+        <translation>モデル</translation>
+    </message>
+    <message>
+        <source>Folder name</source>
+        <translation>フォルダ名</translation>
+    </message>
+    <message>
+        <source>Solver</source>
+        <translation>ソルバ</translation>
+    </message>
+</context>
+<context>
+    <name>Model</name>
+    <message>
+        <source>Output time interval</source>
+        <translation>出力間隔</translation>
+    </message>
+</context>
+<context>
+    <name>ModelAddDialog</name>
+    <message>
+        <source>Add Model</source>
+        <translation>モデルの追加</translation>
+    </message>
+    <message>
+        <source>Folder Name:</source>
+        <translation>フォルダ名</translation>
+    </message>
+    <message>
+        <source>Please input the folder name.</source>
+        <translation>フォルダ名を入力して下さい。</translation>
+    </message>
+    <message>
+        <source>Model</source>
+        <translation>モデル</translation>
+    </message>
+    <message>
+        <source>Please input folder name.</source>
+        <translation>フォルダ名を入力して下さい。</translation>
+    </message>
+    <message>
+        <source>You can use only ASCII characters.</source>
+        <translation>英数字のみ使用できます。</translation>
+    </message>
+    <message>
+        <source>Folder &quot;%1&quot; already exists.</source>
+        <translation>フォルダ「%1」は既に存在します。</translation>
+    </message>
+</context>
+<context>
+    <name>ModelDeleteDialog</name>
+    <message>
+        <source>Delete Model</source>
+        <translation>モデルの削除</translation>
+    </message>
+    <message>
+        <source>Target Folder:</source>
+        <translation>対象フォルダ:</translation>
+    </message>
+    <message>
+        <source>Mode</source>
+        <translation>モード</translation>
+    </message>
+    <message>
+        <source>Keep the content in the folder</source>
+        <translation>フォルダ内のファイルを残す</translation>
+    </message>
+    <message>
+        <source>Delete all files in the folder</source>
+        <translation>フォルダ内のファイルを削除する</translation>
+    </message>
+</context>
+<context>
+    <name>iRICMIMainWindow</name>
+    <message>
+        <source>iRIC-MI 0.0.1</source>
+        <translation>iRIC-MI 0.0.1</translation>
+    </message>
+    <message>
+        <source>Models</source>
+        <translation>モデル</translation>
+    </message>
+    <message>
+        <source>&amp;Add...</source>
+        <translation>追加 (A)...</translation>
+    </message>
+    <message>
+        <source>Add &amp;Existing...</source>
+        <translation>既存のモデルを追加 (&amp;E)...</translation>
+    </message>
+    <message>
+        <source>&amp;Remove...</source>
+        <translation>削除 (&amp;R)...</translation>
+    </message>
+    <message>
+        <source>Connections</source>
+        <translation>接続</translation>
+    </message>
+    <message>
+        <source>&amp;Visualize...</source>
+        <translation>可視化 (&amp;V)...</translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation>ファイル (&amp;F)</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>ヘルプ (&amp;H)</translation>
+    </message>
+    <message>
+        <source>toolBar</source>
+        <translation>toolBar</translation>
+    </message>
+    <message>
+        <source>&amp;Exit</source>
+        <translation>終了 (&amp;X)</translation>
+    </message>
+    <message>
+        <source>&amp;New Project...</source>
+        <translation>新しいプロジェクト (&amp;N)...</translation>
+    </message>
+    <message>
+        <source>Ctrl+N</source>
+        <translation>Ctrl+N</translation>
+    </message>
+    <message>
+        <source>&amp;Open...</source>
+        <translation>開く (&amp;O)...</translation>
+    </message>
+    <message>
+        <source>Ctrl+O</source>
+        <translation>Ctrl+O</translation>
+    </message>
+    <message>
+        <source>&amp;Save</source>
+        <translation>保存 (&amp;S)</translation>
+    </message>
+    <message>
+        <source>Ctrl+S</source>
+        <translation>Ctrl+S</translation>
+    </message>
+    <message>
+        <source>&amp;Property...</source>
+        <translation>プロパティ (&amp;P)...</translation>
+    </message>
+    <message>
+        <source>&amp;Recent Projects</source>
+        <translation>最近のプロジェクト (&amp;R)</translation>
+    </message>
+    <message>
+        <source>&amp;About...</source>
+        <translation>iRIC-MI について (&amp;A)...</translation>
+    </message>
+    <message>
+        <source>&amp;Close</source>
+        <translation>閉じる (&amp;C)</translation>
+    </message>
+    <message>
+        <source>Reload</source>
+        <translation>再読み込み</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>エラー</translation>
+    </message>
+    <message>
+        <source>Saving %1 failed. Please make sure other programs are opening the file, and try again.</source>
+        <translation>%1 の保存に失敗しました。他のプログラムがファイルを開いていないか確認してから再度試して下さい。</translation>
+    </message>
+    <message>
+        <source>Project successfully saved to %1</source>
+        <translation>プロジェクトは %1 に保存されました</translation>
+    </message>
+    <message>
+        <source>Error occured while preparing data for model in &quot;%1&quot;</source>
+        <translation>モデル「%1」用のデータの準備中にエラーが発生しました。</translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation>警告</translation>
+    </message>
+    <message>
+        <source>No existing project folder found in &quot;%1&quot;.</source>
+        <translation>「%1」には既存のプロジェクトフォルダがありません。</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to delete this connection?</source>
+        <translation>本当にこの接続を削除しますか?</translation>
+    </message>
+    <message>
+        <source>Select folder to save project</source>
+        <translation>プロジェクトを保存するフォルダの選択</translation>
+    </message>
+    <message>
+        <source>%1 already exists. Please select another folder.</source>
+        <translation>%1 は既に存在します。別のフォルダを指定して下さい。</translation>
+    </message>
+    <message>
+        <source>Select project file to open</source>
+        <translation>プロジェクトファイルの選択</translation>
+    </message>
+    <message>
+        <source>iRIC-MI project file (iricmi_project.xml)</source>
+        <translation>iRIC-MIプロジェクトファイル (iricmi_project.xml)</translation>
+    </message>
+    <message>
+        <source>The project is modified. Do you want to save?</source>
+        <translation>このプロジェクトは変更されています。保存しますか?</translation>
+    </message>
+    <message>
+        <source>Saving &quot;%1&quot; failed. Please make sure other program is opening the file, and try again.</source>
+        <translation>%1 の保存に失敗しました。他のプログラムがファイルを開いていないか確認してから再度試して下さい。</translation>
+    </message>
+</context>
+<context>
+    <name>iRICMIProject</name>
+    <message>
+        <source>Folder name</source>
+        <translation>フォルダ名</translation>
+    </message>
+    <message>
+        <source>Solver</source>
+        <translation>ソルバ</translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation>説明</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>開く</translation>
+    </message>
+    <message>
+        <source>Output model</source>
+        <translation>値を送信するモデル</translation>
+    </message>
+    <message>
+        <source>Output value</source>
+        <translation>出力値</translation>
+    </message>
+    <message>
+        <source>Input model</source>
+        <translation>値を受け取るモデル</translation>
+    </message>
+    <message>
+        <source>Input value</source>
+        <translation>入力値</translation>
+    </message>
+</context>
+</TS>
