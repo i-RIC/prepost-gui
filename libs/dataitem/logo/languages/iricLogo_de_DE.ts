b5517cc5b7b240be659a73869fa555e61af7f9e8
iric.appveyor <iric.appveyor@gmail.com>
2024-01-23 14:11:59 +0000

    [skip ci] translate_ts.py by iric.appveyor develop_v4-1189

--- libs/dataitem/logo/languages/iricLogo_de_DE.ts
+++ libs/dataitem/logo/languages/iricLogo_de_DE.ts
@@ -1,45 +1,43 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
 <TS version="2.1" language="de_DE">
 <context>
     <name>LogoDataItem</name>
     <message>
-        <location filename="../logodataitem.cpp" line="9"/>
+        <location filename="../logodataitem.cpp" line="9" />
         <source>Logo</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished" />
     </message>
     <message>
-        <location filename="../logodataitem.cpp" line="50"/>
+        <location filename="../logodataitem.cpp" line="50" />
         <source>Logo Setting</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished" />
     </message>
 </context>
 <context>
     <name>LogoSettingEditWidget</name>
     <message>
-        <location filename="../logosettingeditwidget.ui" line="14"/>
+        <location filename="../logosettingeditwidget.ui" line="14" />
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished" />
     </message>
     <message>
-        <location filename="../logosettingeditwidget.ui" line="34"/>
+        <location filename="../logosettingeditwidget.ui" line="34" />
         <source>Size</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished" />
     </message>
     <message>
-        <location filename="../logosettingeditwidget.ui" line="40"/>
+        <location filename="../logosettingeditwidget.ui" line="40" />
         <source>Small</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished" />
     </message>
     <message>
-        <location filename="../logosettingeditwidget.ui" line="47"/>
+        <location filename="../logosettingeditwidget.ui" line="47" />
         <source>Standard</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished" />
     </message>
     <message>
-        <location filename="../logosettingeditwidget.ui" line="57"/>
+        <location filename="../logosettingeditwidget.ui" line="57" />
         <source>Large</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished" />
     </message>
 </context>
 </TS>
--- libs/dataitem/logo/languages/iricLogo_ja_JP.ts
+++ libs/dataitem/logo/languages/iricLogo_ja_JP.ts
@@ -1,15 +1,13 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
 <TS version="2.1" language="ja_JP">
 <context>
     <name>LogoDataItem</name>
     <message>
-        <location filename="../logodataitem.cpp" line="9"/>
+        <location filename="../logodataitem.cpp" line="9" />
         <source>Logo</source>
         <translation>ロゴ</translation>
     </message>
     <message>
-        <location filename="../logodataitem.cpp" line="50"/>
+        <location filename="../logodataitem.cpp" line="50" />
         <source>Logo Setting</source>
         <translation>ロゴ設定</translation>
     </message>
@@ -17,27 +15,27 @@
 <context>
     <name>LogoSettingEditWidget</name>
     <message>
-        <location filename="../logosettingeditwidget.ui" line="14"/>
+        <location filename="../logosettingeditwidget.ui" line="14" />
         <source>Form</source>
         <translation>フォーム</translation>
     </message>
     <message>
-        <location filename="../logosettingeditwidget.ui" line="34"/>
+        <location filename="../logosettingeditwidget.ui" line="34" />
         <source>Size</source>
         <translation>サイズ</translation>
     </message>
     <message>
-        <location filename="../logosettingeditwidget.ui" line="40"/>
+        <location filename="../logosettingeditwidget.ui" line="40" />
         <source>Small</source>
         <translation>小</translation>
     </message>
     <message>
-        <location filename="../logosettingeditwidget.ui" line="47"/>
+        <location filename="../logosettingeditwidget.ui" line="47" />
         <source>Standard</source>
         <translation>標準</translation>
     </message>
     <message>
-        <location filename="../logosettingeditwidget.ui" line="57"/>
+        <location filename="../logosettingeditwidget.ui" line="57" />
         <source>Large</source>
         <translation>大</translation>
     </message>
