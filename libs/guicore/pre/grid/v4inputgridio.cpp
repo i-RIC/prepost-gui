0dffc32b407251fdaa0bf07badc29766a3eb8a7b
Keisuke Inoue <kskinoue@gmail.com>
2024-10-29 12:58:55 +0900

    Bug: iRIC GUI crashes when the CGNS file zone is broken. #1445

--- libs/guicore/pre/grid/v4inputgridio.cpp
+++ libs/guicore/pre/grid/v4inputgridio.cpp
@@ -27,6 +27,11 @@ v4InputGrid* v4InputGridIO::load(const iRICLib::H5CgnsZone& zone, PreProcessorGr
 		grid = loadUnstructured2dGrid(zone, offset, ier);
 	} else {
 		auto size = zone.size();
+		if (size.size() == 0) {
+			*ier = IRIC_DATA_NOT_FOUND;
+			return nullptr;
+		}
+
 		if (size[1] == 1) {
 			grid = loadStructured15DGridWithCrossSection(zone, offset, ier);
 		} else {
