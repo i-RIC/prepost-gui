4d1d4d91185c807cf2d9cf649ac2fb0334e5931c
ymorita125 <181345708+ymorita125@users.noreply.github.com>
2025-01-22 17:29:22 +0900

    GeoLastIODirectory を実装

--- libs/misc/geolastiodirectory.cpp
+++ libs/misc/geolastiodirectory.cpp
@@ -1,6 +1,30 @@
 #include "geolastiodirectory.h"
 
-GeoLastIODirectory::GeoLastIODirectory()
-{
+#include <QDir>
+#include <QFileInfo>
+#include <QSettings>
 
+QString GeoLastIODirectory::get()
+{
+	QSettings settings;
+	auto dir = settings.value("general/geolastiodir", QDir::homePath()).toString();
+	if (dir == "" || ! QDir(dir).exists()) {
+		dir = QDir::homePath();
+	}
+	return dir;
 }
+
+void GeoLastIODirectory::set(const QString& val)
+{
+	QSettings settings;
+	settings.setValue("general/geolastiodir", val);
+}
+
+void GeoLastIODirectory::setFromFilename(const QString& filename)
+{
+	QFileInfo finfo(filename);
+	set(finfo.absolutePath());
+}
+
+GeoLastIODirectory::GeoLastIODirectory()
+{}
--- libs/misc/geolastiodirectory.h
+++ libs/misc/geolastiodirectory.h
@@ -1,10 +1,16 @@
 #ifndef GEOLASTIODIRECTORY_H
 #define GEOLASTIODIRECTORY_H
 
+#include "misc_global.h"
 
-class GeoLastIODirectory
+class MISCDLL_EXPORT GeoLastIODirectory
 {
 public:
+	static QString get();
+	static void set(const QString& val);
+	static void setFromFilename(const QString& filename);
+
+private:
 	GeoLastIODirectory();
 };
 
