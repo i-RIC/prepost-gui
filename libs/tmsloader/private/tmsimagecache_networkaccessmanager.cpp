b6a93e0bb1cfdd64ffe25024c2c7bc2dd7caa261
Keisuke Inoue <kskinoue@gmail.com>
2025-01-24 10:52:31 +0900

    User agent is set to immitate Google Chrome

--- libs/tmsloader/private/tmsimagecache_networkaccessmanager.cpp
+++ libs/tmsloader/private/tmsimagecache_networkaccessmanager.cpp
@@ -134,6 +134,8 @@ void TmsImageCache::NetworkAccessManager::doRegisterRequest(const QString& urlPa
 
 	QNetworkRequest request(qUrl);
 
+	request.setHeader(QNetworkRequest::KnownHeaders::UserAgentHeader, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36");
+
 	auto entry = new Entry();
 	entry->status = Entry::Status::Loading;
 	entry->url = qUrl.toString();
