ed053d2b9b8fd3898728b5ef5652b0ee2dfdfb5d
ymorita125 <181345708+ymorita125@users.noreply.github.com>
2025-01-29 20:35:32 +0900

    ProjectLastIODirectoryを実装

--- libs/misc/projectlastiodirectory.cpp
+++ libs/misc/projectlastiodirectory.cpp
@@ -1,6 +1,30 @@
 #include "projectlastiodirectory.h"
 
-ProjectLastIODirectory::ProjectLastIODirectory()
-{
+#include <QDir>
+#include <QFileInfo>
+#include <QSettings>
 
+QString ProjectLastIODirectory::get()
+{
+	QSettings settings;
+	auto dir = settings.value("general/projectlastiodir", QDir::homePath()).toString();
+	if (dir == "" || ! QDir(dir).exists()) {
+		dir = QDir::homePath();
+	}
+	return dir;
 }
+
+void ProjectLastIODirectory::set(const QString& val)
+{
+	QSettings settings;
+	settings.setValue("general/projectlastiodir", val);
+}
+
+void ProjectLastIODirectory::setFromFilename(const QString& filename)
+{
+	QFileInfo finfo(filename);
+	set(finfo.absolutePath());
+}
+
+ProjectLastIODirectory::ProjectLastIODirectory()
+{}
--- libs/misc/projectlastiodirectory.h
+++ libs/misc/projectlastiodirectory.h
@@ -1,10 +1,16 @@
 #ifndef PROJECTLASTIODIRECTORY_H
 #define PROJECTLASTIODIRECTORY_H
 
+#include "misc_global.h"
 
-class ProjectLastIODirectory
+class MISCDLL_EXPORT ProjectLastIODirectory
 {
 public:
+	static QString get();
+	static void set(const QString& val);
+	static void setFromFilename(const QString& filename);
+
+private:
 	ProjectLastIODirectory();
 };
 
