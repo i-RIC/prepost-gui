e253031da738db8eba0c90e546bcadd11fc6845f
ymorita125 <181345708+ymorita125@users.noreply.github.com>
2025-01-29 13:11:10 +0900

    pre/datamodel/preprocessorgridattributenodedataitem.cpp, preprocessorgridattributeabstractcelldataitem.cpp について、 LastIODirectory から GeoLastIODirectory に切り替えた

--- libs/pre/datamodel/preprocessorgridattributeabstractcelldataitem.cpp
+++ libs/pre/datamodel/preprocessorgridattributeabstractcelldataitem.cpp
@@ -37,7 +37,7 @@
 #include <guicore/solverdef/solverdefinitiongridcomplexattribute.h>
 #include <misc/errormessage.h>
 #include <misc/iricundostack.h>
-#include <misc/lastiodirectory.h>
+#include <misc/geolastiodirectory.h>
 #include <misc/qscreenutil.h>
 #include <misc/qwidgetcontainer.h>
 #include <misc/tpoexporter.h>
@@ -367,7 +367,7 @@ void PreProcessorGridAttributeAbstractCellDataItem::exportToFile()
 		return;
 	}
 
-	QString dir = LastIODirectory::get();
+	QString dir = GeoLastIODirectory::get();
 	QString filter(tr("Topography File (*.tpo)"));
 
 	QString fname = QFileDialog::getSaveFileName(iricMainWindow(), tr("Export as Topography Data"), dir, filter);
@@ -410,7 +410,7 @@ void PreProcessorGridAttributeAbstractCellDataItem::exportToFile()
 		exporter.close();
 		iricMainWindow()->statusBar()->showMessage(tr("Grid condition successfully exported to %1.").arg(QDir::toNativeSeparators(fname)), iRICMainWindowI::STATUSBAR_DISPLAYTIME);
 		QFileInfo finfo(fname);
-		LastIODirectory::set(finfo.absolutePath());
+		GeoLastIODirectory::set(finfo.absolutePath());
 	} catch (ErrorMessage& message) {
 		QMessageBox::critical(iricMainWindow(), tr("Error"), message);
 	}
