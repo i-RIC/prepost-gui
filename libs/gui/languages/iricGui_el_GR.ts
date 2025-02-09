8bad3dc6e1d9424010f70408ca2f01ccb11467a6
Keisuke Inoue <kskinoue@gmail.com>
2025-02-09 12:09:42 +0900

    Dictionary updated

--- libs/gui/languages/iricGui_el_GR.ts
+++ libs/gui/languages/iricGui_el_GR.ts
@@ -2248,6 +2248,21 @@ Option -&gt; Preferences</source>
     </message>
 </context>
 <context>
+    <name>iRICMainWindow::DiscardResultConfirmDialog</name>
+    <message>
+        <source>Warning</source>
+        <translation type="unfinished">Προειδοποίηση</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to discard the grid modification?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Are you sure you want to discard the calculation result?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>iRICMainWindowAboutDialog</name>
     <message>
         <source>About iRIC</source>
