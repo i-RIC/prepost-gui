1451a9d8dbc8836d0298236bdb31f1c5cafd86e0
Keisuke Inoue <kskinoue@gmail.com>
2025-04-20 12:54:33 +0900

    横断測量データをリファクタリング

--- libs/geodata/riversurvey/geodatariversurveyreadonlycreator.h
+++ libs/geodata/riversurvey/geodatariversurveyreadonlycreator.h
@@ -2,22 +2,14 @@
 #define GEODATARIVERSURVEYREADONLYCREATOR_H
 
 #include "gd_riversurvey_global.h"
-#include <guicore/pre/geodata/geodatacreator.h>
+#include "geodatariversurveycreator.h"
 
-class GD_RIVERSURVEY_EXPORT GeoDataRiverSurveyReadOnlyCreator : public GeoDataCreator
+class GD_RIVERSURVEY_EXPORT GeoDataRiverSurveyReadOnlyCreator : public GeoDataRiverSurveyCreator
 {
 	Q_OBJECT
 
 public:
 	GeoDataRiverSurveyReadOnlyCreator();
-
-	QString name(unsigned int index) const override;
-	QString defaultCaption(unsigned int index) const override;
-
-	bool isCompatibleWith(SolverDefinitionGridAttribute* condition) const override;
-
-	GeoData* create(ProjectDataItem* parent, SolverDefinitionGridAttribute* condition) override;
-	GeoData* restore(const QDomNode& node, ProjectDataItem* parent, SolverDefinitionGridAttribute* condition) override;
 
 	bool isCreatable() const override;
 	bool isReadOnly() const override;
