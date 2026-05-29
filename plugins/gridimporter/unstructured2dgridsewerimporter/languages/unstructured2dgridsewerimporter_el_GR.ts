6df734e6991dc43f2e6c145f89d94f719f837b6b
Keisuke Inoue <kskinoue@gmail.com>
2026-05-29 17:35:51 +0900

    辞書ファイルを追加

---
+++ plugins/gridimporter/unstructured2dgridsewerimporter/languages/unstructured2dgridsewerimporter_el_GR.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="el_GR">
+<context>
+    <name>Unstructured2dGridSewerImporter</name>
+    <message>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="44"/>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="212"/>
+        <source>Select file containing culvert data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="44"/>
+        <source>CSV file containing culvert data (*.csv)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="212"/>
+        <source>Shapefile containing culvert data (*.shp)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="225"/>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="239"/>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="287"/>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="301"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="225"/>
+        <source>The shape type contained in %1 is not point.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="239"/>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="301"/>
+        <source>The number of data mismatches between shp file and dbf file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="287"/>
+        <source>The shape type contained in %1 is not line.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="406"/>
+        <source>CSV file containing manhole data (*.csv)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="407"/>
+        <source>Shapefile containing manhole data (*.shp)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="413"/>
+        <source>Sewer data</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ plugins/gridimporter/unstructured2dgridsewerimporter/languages/unstructured2dgridsewerimporter_ja_JP.ts
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="utf-8"?>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>Unstructured2dGridSewerImporter</name>
+    <message>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="44"/>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="212"/>
+        <source>Select file containing culvert data</source>
+        <translation>管渠データを含むファイルの選択</translation>
+    </message>
+    <message>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="44"/>
+        <source>CSV file containing culvert data (*.csv)</source>
+        <translation>管渠データを含むCSVファイル (*.csv)</translation>
+    </message>
+    <message>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="212"/>
+        <source>Shapefile containing culvert data (*.shp)</source>
+        <translation>管渠データを含むShapeファイル (*.shp)</translation>
+    </message>
+    <message>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="225"/>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="239"/>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="287"/>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="301"/>
+        <source>Error</source>
+        <translation>エラー</translation>
+    </message>
+    <message>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="225"/>
+        <source>The shape type contained in %1 is not point.</source>
+        <translation>%1 に含まれるシェイプタイプはポイントではありません。</translation>
+    </message>
+    <message>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="239"/>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="301"/>
+        <source>The number of data mismatches between shp file and dbf file.</source>
+        <translation>shpファイルとdbfファイルのデータ数が一致しません。</translation>
+    </message>
+    <message>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="287"/>
+        <source>The shape type contained in %1 is not line.</source>
+        <translation>%1 に含まれるシェイプタイプはラインではありません。</translation>
+    </message>
+    <message>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="406"/>
+        <source>CSV file containing manhole data (*.csv)</source>
+        <translation>人孔データを含むCSVファイル (*.csv)</translation>
+    </message>
+    <message>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="407"/>
+        <source>Shapefile containing manhole data (*.shp)</source>
+        <translation>人孔データを含むShapeファイル (*.shp)</translation>
+    </message>
+    <message>
+        <location filename="../unstructured2dgridsewerimporter.cpp" line="413"/>
+        <source>Sewer data</source>
+        <translation>下水道データ</translation>
+    </message>
+</context>
+</TS>
